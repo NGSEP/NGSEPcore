86a41b973728f8cc85c27b63d183cee1add370f7
natal <natal@LAPTOP-HR83VCPJ.uniandes.edu.co>
2017-05-03 14:17:33 -0500

    Hamming distance

--- src/ngsep/variants/HaplotypeBlock.java
+++ src/ngsep/variants/HaplotypeBlock.java
@@ -54,14 +54,33 @@ public class HaplotypeBlock {
 	}
 	
 	/**
-	 * 	
+	 * Returns Hamming distance between two fragments
+	 * <b> pre: </b> The matrix of fragments has been initialized.
 	 * @param row1. Row1 is bigger than row2
-	 * @param row2
-	 * @return
+	 * @param row2.
+	 * @exception row1 must be bigger than row2.
+	 * @return hamming distance between two fragments.
 	 */
-	public int getHammingDistance(int row1, int row2)
+	public int getHammingDistance(int row1, int row2) 
 	{
-		return 0;
+		int distance = 0;
+		HaplotypeFragment fragment1 = matrix.get(row1);
+		HaplotypeFragment fragment2 = matrix.get(row2);
+		byte[] callsF1 = fragment1.getCalls();
+		byte[] callsF2 = fragment2.getCalls();
+		for(int i = fragment1.getFirstColum(); i < callsF1.length ; i++ )
+		{
+			byte callF1 = fragment1.getCall(i);
+			for(int j = fragment2.getFirstColum(); j < callsF2.length ; j++)
+			{
+				byte callF2 = fragment2.getCall(j);
+				if(callF1 != callF2)
+				{
+					distance ++;
+				}
+			}
+		}
+		return distance;
 	}
 	/**
 	 * 
@@ -71,7 +90,24 @@ public class HaplotypeBlock {
 	 */
 	public boolean overlap(int row1, int row2)
 	{
-		return false;
+		boolean overlap = false;		
+		HaplotypeFragment fragment2 = matrix.get(row2);
+		HaplotypeFragment fragment1 = matrix.get(row1);
+		int initPosF1 = fragment1.getFirstColum();
+		int initPosF2 = fragment2.getFirstColum();
+		int lengthF1 = fragment1.getCalls().length;
+		int lengthF2 = fragment2.getCalls().length;
+		int lastPosF2 = initPosF2 + lengthF2 -1;
+		for (int i = initPosF1; i < lengthF1 ; i++ )
+		{
+			byte callF1 = fragment1.getCall(i);
+			for(int j = lastPosF2 ; j > initPosF2; j--)
+			{
+				byte callF2 = fragment2.getCall(j);
+			}
+		}
+		
+		return overlap;
 	}
 	
 
