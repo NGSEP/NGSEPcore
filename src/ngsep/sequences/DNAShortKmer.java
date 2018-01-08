187a94c4ad6b193e95ce46a180ba13400111f908
jorge <jorge@jorge-Spectre-x360>
2018-01-07 21:19:09 -0500

    Added main method to test the behavior of different Collections of k-mers

--- src/ngsep/sequences/DNAShortKmer.java
+++ src/ngsep/sequences/DNAShortKmer.java
@@ -1,6 +1,17 @@
 package ngsep.sequences;
 
-public class DNAShortKmer implements CharSequence, Comparable<DNAShortKmer> {
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Random;
+import java.util.TreeSet;
+
+public class DNAShortKmer implements CharSequence, Comparable<DNAShortKmer>, Serializable {
+	private static final long serialVersionUID = 1L;
 	private long index;
 	private byte length;
 	private static final DNASequence EMPTYDNASEQ = new DNASequence();
@@ -49,5 +60,67 @@ public class DNAShortKmer implements CharSequence, Comparable<DNAShortKmer> {
 		if(length!=kmer2.length) return length - kmer2.length;
 		if(index == kmer2.index) return 0;
 		return (index<kmer2.index)?-1:1;
+	}
+	/**
+	 * Main method to test the number of short k-mers that can be added to a list
+	 * @param args
+	 */
+	public static void main(String[] args) throws Exception {
+		int numKmers = Integer.parseInt(args[0]);
+		int length = Integer.parseInt(args[1]);
+		int collectiontype = Integer.parseInt(args[2]);
+		Collection<DNAShortKmer> kmers = new ArrayList<>();
+		boolean isList = true;
+		if (collectiontype == 1) {
+			kmers = new LinkedList<>();
+		} else if (collectiontype == 2) {
+			kmers = new TreeSet<>();
+			isList = false;
+		} else if (collectiontype == 3) {
+			kmers = new HashSet<>();
+			isList = false;
+		}
+		
+		char [] kmerC = new char [length];
+		Random r = new Random();
+		long time1 = System.currentTimeMillis();
+		for(int i=0;i<numKmers;i++) {
+			//Create kmer
+			for(int j=0;j<kmerC.length;j++) {
+				int bpI = r.nextInt(4);
+				kmerC[j] = DNASequence.BASES_STRING.charAt(bpI);
+			}
+			String kmerStr = new String(kmerC);
+			DNAShortKmer kmer = new DNAShortKmer(kmerStr);
+			kmers.add(kmer);
+			if((i+1)%100000==0) System.out.println("Created "+kmers.size()+" kmers");
+		}
+		long time2 = System.currentTimeMillis();
+		System.out.println("Time creating k-mers: "+(time2-time1)+" Number of k-mers: "+kmers.size());
+		time1 = time2;
+		if(isList) {
+			Collections.sort(((List<DNAShortKmer>)kmers));
+			time2 = System.currentTimeMillis();
+			System.out.println("Time sorting k-mers: "+(time2-time1));
+			time1 = time2;
+		}
+		//Search experiment
+		for(int i=0;i<1000000;i++) {
+			//Create kmer
+			for(int j=0;j<kmerC.length;j++) {
+				int bpI = r.nextInt(4);
+				kmerC[j] = DNASequence.BASES_STRING.charAt(bpI);
+			}
+			String kmerStr = new String(kmerC);
+			DNAShortKmer kmer = new DNAShortKmer(kmerStr);
+			if(isList) {
+				Collections.binarySearch((List<DNAShortKmer>)kmers, kmer);
+			} else {
+				kmers.contains(kmer);
+			}
+		}
+		time2 = System.currentTimeMillis();
+		System.out.println("Time searching 1000000 k-mers: "+(time2-time1));
+		time1 = time2;
 	}
 }
