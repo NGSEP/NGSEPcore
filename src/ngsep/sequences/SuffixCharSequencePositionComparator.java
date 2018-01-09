caffb4baa0c535f5d36f6ee849006849b13b17db
jorge <jorge@jorge-Spectre-x360>
2018-01-09 15:56:12 -0500

    Reduced code

--- src/ngsep/sequences/SuffixCharSequencePositionComparator.java
+++ src/ngsep/sequences/SuffixCharSequencePositionComparator.java
@@ -1,47 +1,29 @@
 package ngsep.sequences;
 import java.util.Comparator;
-import java.lang.Math;
 
 public class SuffixCharSequencePositionComparator implements Comparator<Integer> 
 {
 	private CharSequence word;
+	private int length;
 	public SuffixCharSequencePositionComparator(CharSequence word) 
 	{
 		super();
 		this.word = word;
+		this.length = word.length();
 	}
 	
 	@Override
-	public int compare(Integer o1, Integer o2) 
-	{
-		//o1>o2?
-		int r = 0;
-		int max = Math.max(o1, o2);
-		for (int i = 0; max < word.length(); i++) 
-		{
-			if(word.charAt(o1)>word.charAt(o2))
-			{
-				return 1;
-			}
-			else if(word.charAt(o1)<word.charAt(o2))
-			{
-				return -1;
-			}
+	public int compare(Integer o1, Integer o2) {
+		while (o1<length && o2<length) {
+			char c1 = word.charAt(o1);
+			char c2 = word.charAt(o2); 
+			if(c1!=c2) return c1-c2;
 			o1++;
 			o2++;
-			max++;
-			if(o1==word.length() & max< word.length())
-			{
-				return -1;
-			}
-			if(o2==word.length() & max< word.length())
-			{
-				return 1;
-			}
-			//TODO SI SE ACABA 1 ES MENOR....
-			
 		}
-		return r;
+		if( o2<length) return -1;
+		if( o1<length) return 1;
+		return 0;
 	}
 
 }
