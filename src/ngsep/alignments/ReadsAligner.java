5301ee87b39691ae27301956ba40d48df501a4ab
jorge <jorge@jorge-Spectre-x360>
2018-01-09 23:08:07 -0500

    Improved output and logging information

--- src/ngsep/alignments/ReadsAligner.java
+++ src/ngsep/alignments/ReadsAligner.java
@@ -1,3 +1,22 @@
+/*******************************************************************************
+ * NGSEP - Next Generation Sequencing Experience Platform
+ * Copyright 2016 Jorge Duitama
+ *
+ * This file is part of NGSEP.
+ *
+ *     NGSEP is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     NGSEP is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with NGSEP.  If not, see <http://www.gnu.org/licenses/>.
+ *******************************************************************************/
 package ngsep.alignments;
 
 import java.io.IOException;
@@ -11,98 +30,99 @@ import ngsep.sequences.FMIndex;
 import ngsep.sequences.RawRead;
 import ngsep.sequences.io.FastqFileReader;
 
+/**
+ * Program to align reads to a refernece genome
+ * @author German Andrade
+ * @author Jorge Duitama
+ */
 public class ReadsAligner {
 
 	public static void main(String[] args) throws Exception 
 	{
 		ReadsAligner instance = new ReadsAligner();
 		int i = CommandsDescriptor.getInstance().loadOptions(instance, args);
-		if(i<0) return;
 		String fMIndexFile = args[i++];
 		String readsFile = args[i++];
-		
-		instance.alignReads(fMIndexFile, readsFile, System.out);
-			
+		String outFile = args[i++];
+		try (PrintStream out = new PrintStream(outFile)){
+			instance.alignReads(fMIndexFile, readsFile, out);
+		}
 	}
 
 	public void alignReads( String fMIndexFile, String readsFile, PrintStream out) throws IOException {
 		FMIndex fMIndex = FMIndex.loadFromBinaries(fMIndexFile);
+		int totalReads = 0;
+		int readsAligned = 0;
+		int uniqueAlignments=0;
+		long time = System.currentTimeMillis();
 		try (FastqFileReader reader = new FastqFileReader(readsFile)) {
 			Iterator<RawRead> it = reader.iterator();
 			while(it.hasNext()) {
 				RawRead read = it.next();
-				List<ReadAlignment> alignments = fMIndex.search(read.getCharacters().toString());
-				for (ReadAlignment aln: alignments) {
-					String readSeq = read.getSequenceString();
-					String qual = read.getQualityScores();
-					if(aln.isNegativeStrand()) {
-						readSeq = DNAMaskedSequence.getReverseComplement(readSeq).toString();
-						qual = new StringBuilder(qual).reverse().toString();
-					}
-					out.println(
-							//1.query name
-							read.getName()+"\t"+
-									
-							//2.Flag
-							aln.getFlags()+"\t"+
-							
-							//3.reference sequence name
-							aln.getSequenceName()+"\t"+
-							
-							//4.POS
-							aln.getFirst()+"\t"+
-							
-							//5.MAPQ
-							"255\t"+
-							
-							//6.CIGAR
-							read.getLength()+"M\t"+
-							
-							//7. RNEXT
-							"*\t"+
-							
-							//8. PNEXT
-							"0\t"+
-							
-							//9. TLEN
-							"0\t"+
-							
-							//10. SEQ
-							readSeq+"\t"+
-							
-							//11. QUAL
-							qual
-							
-							);
-				}
+				int numAlns = alignRead(fMIndex, read, out);
+				totalReads++;
+				if(numAlns>0) readsAligned++;
+				if(numAlns==1) uniqueAlignments++;
 			}
 		}
+		System.out.println("Total reads: "+totalReads);
+		System.out.println("Reads aligned: "+readsAligned);
+		System.out.println("Unique alignments: "+uniqueAlignments);
+		System.out.println("Overall alignment rate: "+(100.0*readsAligned/(double)totalReads)+"%");
+		double seconds = (System.currentTimeMillis()-time);
+		seconds /=1000;
+		System.out.println("Time: "+seconds+" seconds");
 		
-		
-
-		out.println();
-	}
-	
-//	private static void write(String path, String content) throws IOException 
-//	{
-//
-//        File file = new File(path+".sam");
-//
-//        // if file doesnt exists, then create it
-//        if (!file.exists()) {
-//            file.createNewFile();
-//        }
-//
-//        FileWriter fw = new FileWriter(file.getAbsoluteFile(),true);
-//        BufferedWriter bw = new BufferedWriter(fw);
-//        bw.write(content);
-//        bw.close();
-//	}
-
-	public FMIndex loadIndex(String fMIndexFile) throws IOException 
-	{
-		FMIndex f = FMIndex.loadFromBinaries(fMIndexFile);
-		return f;
 	}
 
+	private int alignRead(FMIndex fMIndex, RawRead read, PrintStream out) {
+		List<ReadAlignment> alignments = fMIndex.search(read.getCharacters().toString());
+		int i=0;
+		for (ReadAlignment aln: alignments) {
+			String readSeq = read.getSequenceString();
+			String qual = read.getQualityScores();
+			if(aln.isNegativeStrand()) {
+				readSeq = DNAMaskedSequence.getReverseComplement(readSeq).toString();
+				qual = new StringBuilder(qual).reverse().toString();
+			}
+			if(i>0) aln.setFlags(aln.getFlags()+ReadAlignment.FLAG_SECONDARY);
+			out.println(
+					//1.query name
+					read.getName()+"\t"+
+							
+					//2.Flag
+					aln.getFlags()+"\t"+
+					
+					//3.reference sequence name
+					aln.getSequenceName()+"\t"+
+					
+					//4.POS
+					aln.getFirst()+"\t"+
+					
+					//5.MAPQ
+					"255\t"+
+					
+					//6.CIGAR
+					read.getLength()+"M\t"+
+					
+					//7. RNEXT
+					"*\t"+
+					
+					//8. PNEXT
+					"0\t"+
+					
+					//9. TLEN
+					"0\t"+
+					
+					//10. SEQ
+					readSeq+"\t"+
+					
+					//11. QUAL
+					qual
+					
+					);
+			i++;
+		}
+		return alignments.size();
+	}
 }
--- src/ngsep/genome/GenomeIndexer.java
+++ src/ngsep/genome/GenomeIndexer.java
@@ -1,3 +1,22 @@
+/*******************************************************************************
+ * NGSEP - Next Generation Sequencing Experience Platform
+ * Copyright 2016 Jorge Duitama
+ *
+ * This file is part of NGSEP.
+ *
+ *     NGSEP is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     NGSEP is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with NGSEP.  If not, see <http://www.gnu.org/licenses/>.
+ *******************************************************************************/
 package ngsep.genome;
 
 import java.io.IOException;
@@ -5,26 +24,35 @@ import java.io.IOException;
 import ngsep.main.CommandsDescriptor;
 import ngsep.sequences.FMIndex;
 
+/**
+ * Program that build the FM-index related to a genome
+ * @author German Andrade
+ * @author Jorge Duitama
+ */
 public class GenomeIndexer 
 {
 
 	private int tallyDistance;
-	public static void main(String[] args) throws Exception 
-	{
-		long t = System.currentTimeMillis();
+	public static void main(String[] args) throws Exception  {
 		GenomeIndexer instance = new GenomeIndexer();
 		int i = CommandsDescriptor.getInstance().loadOptions(instance, args);
 		String genomeFile=args[i++];
 		String outputFile=args[i++];
 		instance.createIndex (genomeFile,outputFile);
-		System.out.println(System.currentTimeMillis()-t);
+		
 	}
 
 	public void createIndex(String genomeFile, String outputFile) throws IOException {
-		// TODO Auto-generated method stub
+		System.out.println("Building index for genome in file "+genomeFile);
+		long time = System.currentTimeMillis();
 		FMIndex fMIndex= new FMIndex();
 		fMIndex.loadGenome(genomeFile);
+		double seconds = (System.currentTimeMillis()-time);
+		seconds /=1000;
+		System.out.println("Built index in "+seconds+" seconds. Saving in "+outputFile);
 		fMIndex.save(outputFile);
+		System.out.println("Process completed");
+		
 	}
 
 	public int getTallyDistance() {
