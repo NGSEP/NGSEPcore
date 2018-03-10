d2638956b64d70929dfccf91e99db8c950ff8105
jorge <jorge@jorge-0420006391>
2018-03-10 12:43:11 -0500

    Fixed errors

--- src/ngsep/benchmark/BiparentalHaploidGoldStandardBuilder.java
+++ src/ngsep/benchmark/BiparentalHaploidGoldStandardBuilder.java
@@ -4,16 +4,20 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.io.PrintStream;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.TreeMap;
 
+import ngsep.genome.GenomicRegionSortedCollection;
 import ngsep.genome.ReferenceGenome;
-import ngsep.variants.AlleleCompatibilityGenomicVariantComparator;
+import ngsep.math.CountsRankHelper;
+import ngsep.sequences.QualifiedSequence;
+import ngsep.sequences.QualifiedSequenceList;
 import ngsep.variants.CalledGenomicVariant;
 import ngsep.variants.CalledGenomicVariantImpl;
 import ngsep.variants.GenomicVariant;
-import ngsep.variants.GenomicVariantAnnotation;
 import ngsep.variants.GenomicVariantImpl;
 import ngsep.vcf.VCFFileHeader;
 import ngsep.vcf.VCFFileReader;
@@ -21,167 +25,234 @@ import ngsep.vcf.VCFFileWriter;
 import ngsep.vcf.VCFRecord;
 
 public class BiparentalHaploidGoldStandardBuilder {
-	private List<String> filesParent1=new ArrayList<>();
-	private List<String> filesParent2=new ArrayList<>();
+	private Map<String,String> files = new TreeMap<>();
+	private int filesP1;
+	private int filesP2;
 	private ReferenceGenome genome;
+	private QualifiedSequenceList sequenceNames;
+	private GenomicRegionSortedCollection<CalledGenomicVariant> allCalls;
+	private int posPrint = 0;
 	public static void main(String[] args) throws Exception {
 		BiparentalHaploidGoldStandardBuilder instance = new BiparentalHaploidGoldStandardBuilder();
-		instance.genome = new ReferenceGenome(args[0]);
+		instance.loadGenome (args[0]);
 		instance.loadDescriptor(args[1]);
 		try (PrintStream out = new PrintStream(args[2])) {
 			instance.buildGoldStandardF1(out);
 		}
 	}
+	public void loadGenome(String filename) throws IOException {	
+		genome = new ReferenceGenome(filename);
+		sequenceNames = genome.getSequencesMetadata();
+		allCalls = new GenomicRegionSortedCollection<>(sequenceNames);
+	}
 	public void loadDescriptor(String filename) throws IOException {
 		try (FileReader reader = new FileReader(filename); 
 			 BufferedReader in = new BufferedReader(reader)) {
+			int i=0;
+			int j=0;
 			String line = in.readLine();
 			while (line != null) {
 				String [] items = line.split("\t");
-				if("1".equals(items[0])) filesParent1.add(items[1]);
-				if("2".equals(items[0])) filesParent2.add(items[1]);
+				String fileId=null;
+				if("1".equals(items[0])) {
+					fileId = "P1_"+i+"_"+items[1];
+					i++;
+				}
+				if("2".equals(items[0])) {
+					fileId = "P2_"+j+"_"+items[1];
+					j++;
+				}
+				if(fileId!=null) files.put(fileId, items[1]);
 				line = in.readLine();
 			}
+			filesP1 = i;
+			filesP2 = j;
 		}
 	}
-	public void buildGoldStandardF1(PrintStream out) throws IOException{
-		List<String> allFiles = new ArrayList<>();
-		allFiles.addAll(filesParent1);
-		allFiles.addAll(filesParent2);
-		int n = allFiles.size();
+	public void buildGoldStandardF1(PrintStream out) throws IOException {
+		for(String fileId:files.keySet()) {
+			loadFileCalls(files.get(fileId),fileId);
+		}
 		VCFFileWriter writer = new VCFFileWriter();
-		List<VCFFileReader> readers = new ArrayList<VCFFileReader>();
-		List<Iterator<VCFRecord>> iterators = new ArrayList<Iterator<VCFRecord>>();
-		VCFFileHeader outHeader = VCFFileHeader.makeDefaultEmptyHeader();
-		outHeader.addDefaultSample("GoldStandardF1");
-		VCFRecord [] calls = new VCFRecord [n];
-		for(int i=0;i<n;i++) {
-			VCFFileReader reader = new VCFFileReader(allFiles.get(i));
-			readers.add(reader);
-			Iterator<VCFRecord> it = reader.iterator();
-			if(it.hasNext()) calls[i] = it.next();
-			else calls[i] = null;
-			iterators.add(it);
-		}
-		AlleleCompatibilityGenomicVariantComparator comparator = new AlleleCompatibilityGenomicVariantComparator(genome);
-		writer.printHeader(outHeader, out);
-		while (true){
-			VCFRecord record = createNextRecord(iterators,calls,filesParent1.size(), comparator,outHeader);
-			if(record == null) break;
-			writer.printVCFRecord(record, out);
-		}
-		for(VCFFileReader reader:readers) reader.close();
-	}
-	private VCFRecord createNextRecord(List<Iterator<VCFRecord>> iterators, VCFRecord[] fileRecords, int firstIdxP2, AlleleCompatibilityGenomicVariantComparator comparator, VCFFileHeader outHeader) {
-		GenomicVariant nextVariant = null;
-		int n = fileRecords.length;
-		//Look for next variant
-		for(int i=0;i<n;i++) {
-			VCFRecord nextRecord = fileRecords[i];
-			if(nextRecord!=null) {
-				if(nextVariant==null || comparator.compare(nextRecord.getVariant(), nextVariant)<0) {
-					nextVariant = nextRecord.getVariant();
+		VCFFileHeader header = VCFFileHeader.makeDefaultEmptyHeader();
+		header.addDefaultSample("GSBiparentalHaploid");
+		writer.printHeader(header, out);
+		for(QualifiedSequence sequence: sequenceNames) {
+			String seqName = sequence.getName();
+			List<CalledGenomicVariant> callsSequence = allCalls.getSequenceRegions(seqName).asList();
+			int first = 0;
+			int last = 0;
+			List<CalledGenomicVariant> clusterCalls=new ArrayList<>();
+			for(CalledGenomicVariant call:callsSequence) {
+				if(call.getFirst()>last) {
+					VCFRecord record = processCluster(seqName,first,last,clusterCalls, header);
+					if(record!=null) writer.printVCFRecord(record, out);
+					printReferenceCalls(seqName, last+1,call.getFirst()-1, header, writer,out);
+					first = call.getFirst();
+					clusterCalls.clear();
 				}
+				clusterCalls.add(call);
+				last = Math.max(last, call.getLast());
 			}
+			VCFRecord record = processCluster(seqName,first,last,clusterCalls,header);
+			if(record!=null) writer.printVCFRecord(record, out);
+			printReferenceCalls(seqName,last+1,sequence.getLength(),header, writer,out);
+			
 		}
-		if(nextVariant == null) return null;
-		List<CalledGenomicVariant> outCalls = new ArrayList<CalledGenomicVariant>();
-		boolean [] nextVarSupport = new boolean[n];
-		List<GenomicVariant> consistentVars = new ArrayList<>();
-		Arrays.fill(nextVarSupport, false);
-		for(int i=0;i<n;i++) {
-			VCFRecord record = fileRecords[i];
-			if(record != null) {
-				GenomicVariant var2 = record.getVariant();
-				if(comparator.compare(nextVariant, var2)==0) {
-					nextVarSupport[i] = true;
-					consistentVars.add(var2);
-					Iterator<VCFRecord> it = iterators.get(i);
-					if(it.hasNext()) fileRecords[i] = it.next();
-					else fileRecords[i] = null;
-				}
-			}
-		}
-		GenomicVariant outVariant = buildMergedVariant(consistentVars, comparator);
-		CalledGenomicVariant outCall = decideCall(outVariant, nextVarSupport,firstIdxP2);
-		outCalls.add(outCall);
-		VCFRecord answer = new VCFRecord(nextVariant, new ArrayList<String>(), new ArrayList<GenomicVariantAnnotation>(), VCFRecord.DEF_FORMAT_ARRAY_MINIMAL, outCalls, outHeader);
-		return answer;
-	}
-	/**
-	 * Merging of consistent variants in the variant that will make it to the 
-	 * @param consistentVars
-	 * @return GenomicVariant Merged variant
-	 */
-	private GenomicVariant buildMergedVariant(List<GenomicVariant> consistentVars, AlleleCompatibilityGenomicVariantComparator comparator) {
-		int first = -1;
-		int last = -1;
-		short quality = 0;
-		String sequenceName = null;
-		byte type= GenomicVariant.TYPE_UNDETERMINED;
-		for (GenomicVariant gv: consistentVars) {
-			sequenceName = gv.getSequenceName();
-			if(first == -1 || first > gv.getFirst()) {
-				first = gv.getFirst();
-			}
-			if(last == -1 || last < gv.getLast()) {
-				last = gv.getLast();
-			}
-			if(quality<gv.getVariantQS()) quality = gv.getVariantQS();
-			type = GenomicVariantImpl.mergeType(type, gv.getType());
-		}
-		List<String> alleles;
-		if(first == last) {
-			alleles = Arrays.asList(consistentVars.get(0).getAlleles());
-		} else {
-			alleles = buildAlleleStrings(consistentVars.get(0), first, last);
-		}
-		GenomicVariant gv = new GenomicVariantImpl(sequenceName, first, alleles);
-		gv.setType(type);
-		gv.setVariantQS(quality);
-		return gv;
+		
+		
+
 	}
 	
-	private List<String> buildAlleleStrings(GenomicVariant v, int first, int last) {
-		String seqName = v.getSequenceName();
-		CharSequence left = null;
-		if(first <=v.getFirst()-1) left = genome.getReference(seqName, first, v.getFirst()-1);
-		CharSequence right = null;
-		if(last>=v.getLast()+1) right = genome.getReference(seqName, v.getLast()+1, last);
-		List<String> allelesList = new ArrayList<>();
-		String [] alleles = v.getAlleles();
-		for(int i=0;i<alleles.length;i++) {
-			String allele = "";
-			if(left!=null)allele+=left;
-			allele+=alleles[i];
-			if(right!=null)allele+=right;
-			allelesList.add(allele.toUpperCase());
-		}
-		return allelesList;
-	}
-	private CalledGenomicVariant decideCall(GenomicVariant variant, boolean[] nextVarSupport, int firstIdxP2) {
-		int supportP1 = 0;
-		int supportP2 = 0;
-		int nP1 = firstIdxP2;
-		int nP2 = nextVarSupport.length-nP1;
-		
-		for(int i=0;i<nextVarSupport.length;i++) {
-			if(nextVarSupport[i]) {
-				if(i<firstIdxP2) supportP1++;
-				else supportP2++;
+	
+	private void loadFileCalls(String filename, String sampleId) throws IOException {
+		System.out.println("Loading calls from file: "+filename);
+		try (VCFFileReader reader = new VCFFileReader(filename)) {
+			Iterator<VCFRecord> it = reader.iterator();
+			while(it.hasNext()) {
+				VCFRecord record = it.next();
+				CalledGenomicVariant call = record.getCalls().get(0);
+				call.setSampleId(sampleId);
+				allCalls.add(call);
 			}
 		}
-		boolean aP1 = (supportP1>=nP1-1);
-		boolean aP2 = (supportP2>=nP2-1);
-		byte [] indexesCalledAlleles = new byte[0];
-		if ( aP1 && aP2 ) {
-			indexesCalledAlleles = new byte[1];
-			indexesCalledAlleles[0]=1;
-		} else if ((aP1 && supportP2==0) || (aP2 && supportP1==0) ) {
-			indexesCalledAlleles = new byte[2];
-			indexesCalledAlleles[0]=0;
-			indexesCalledAlleles[1]=1;
+	}
+	private void printReferenceCalls(String sequenceName, int first, int last, VCFFileHeader header, VCFFileWriter writer, PrintStream out) {
+		if(last<first) return;
+		CharSequence refAllele = genome.getReference(sequenceName,first,last);
+		if(refAllele==null) return;
+		if(refAllele.length()==0) throw new RuntimeException("Empty reference at "+sequenceName+":"+first+"-"+last);
+		List<String> alleles = new ArrayList<>();
+		alleles.add(refAllele.toString());
+		GenomicVariant refVar = new GenomicVariantImpl(sequenceName, first, last, alleles);
+		byte [] calledAlleles = {0};
+		CalledGenomicVariantImpl call = new CalledGenomicVariantImpl(refVar, calledAlleles);
+		writer.printVCFRecord(new VCFRecord(refVar, VCFRecord.DEF_FORMAT_ARRAY_MINIMAL, call, header), out);
+	}
+	private VCFRecord processCluster(String sequenceName, int first, int last, List<CalledGenomicVariant> clusterCalls, VCFFileHeader header) {
+		if(last==0) return null;
+		//Extract alleles
+		CharSequence refAlleleC = genome.getReference(sequenceName,first,last);
+		if(refAlleleC==null) return null;
+		String refAllele = refAlleleC.toString();
+		if(refAllele.length()==0) throw new RuntimeException("Empty reference at "+sequenceName+":"+first+"-"+last+" clusterCalls: "+serializeCalls(clusterCalls));
+		List<String> alleles = new ArrayList<>();
+		alleles.add(refAllele);
+		Set<String> fileIds = files.keySet();
+		CountsRankHelper<Byte> countsP1 = new CountsRankHelper<>();
+		CountsRankHelper<Byte> countsP2 = new CountsRankHelper<>();
+		
+		for(String fileId:fileIds) {
+			String call = makeCall(sequenceName, first, last, refAllele, clusterCalls, fileId);
+			if(call==null) continue;
+			int j = alleles.indexOf(call);
+			if(j<0) {
+				j = alleles.size();
+				if(first == posPrint) System.out.println("For "+sequenceName+":"+first+"-"+last+". Adding allele: "+call+" from file: "+fileId);
+				alleles.add(call);
+			}
+			if(fileId.startsWith("P1")) {
+				countsP1.add((byte)j);
+			} else {
+				countsP2.add((byte)j);
+			}
 		}
-		return new CalledGenomicVariantImpl(variant, indexesCalledAlleles);
+		byte aP1 = -1;
+		if(countsP1.getNumDifferent()>0) {
+			Map.Entry<Byte, Integer> bestP1 = countsP1.selectBest(1).entrySet().iterator().next();
+			if(bestP1.getValue()>=filesP1-2) aP1 = bestP1.getKey(); 
+			if(first == posPrint) System.out.println("For "+sequenceName+":"+first+"-"+last+". Best allele P1: "+bestP1.getKey()+" votes: "+bestP1.getValue()+" aP1: "+aP1);
+		}
+		byte aP2 = -1;
+		if(countsP2.getNumDifferent()>0) {
+			Map.Entry<Byte, Integer> bestP2 = countsP2.selectBest(1).entrySet().iterator().next();
+			if(bestP2.getValue()>=filesP2-2) aP2 = bestP2.getKey(); 
+			if(first == posPrint) System.out.println("For "+sequenceName+":"+first+"-"+last+". Best allele P2: "+bestP2.getKey()+" votes: "+bestP2.getValue()+" aP2: "+aP2);
+		}
+		List<String> finalAlleles = new ArrayList<>();
+		finalAlleles.add(refAllele);
+		
+		if(aP1>0) finalAlleles.add(alleles.get(aP1));
+		if(aP2>0 && aP2!=aP1) finalAlleles.add(alleles.get(aP2));
+		byte [] calledAlleles;
+		if(aP1<0 || aP2<0) {
+			calledAlleles = new byte[0];
+		} else if (aP1==aP2) {
+			calledAlleles = new byte[1];
+			calledAlleles[0] = (byte) (aP1==0?0:1);
+		} else {
+			calledAlleles = new byte[2];
+			calledAlleles[0] = (byte) (aP1==0?0:1);
+			calledAlleles[1] = (byte) (aP2==0?0:finalAlleles.size()-1);
+			if(calledAlleles[0]> calledAlleles[1]) {
+				byte tmp = calledAlleles[0];
+				calledAlleles[0] = calledAlleles[1];
+				calledAlleles[1] = tmp;
+			}
+		}
+		
+		GenomicVariantImpl outVariant = new GenomicVariantImpl(sequenceName, first, last, finalAlleles);
+		CalledGenomicVariantImpl outCall = new CalledGenomicVariantImpl(outVariant, calledAlleles);
+		int [] format = VCFRecord.DEF_FORMAT_ARRAY_MINIMAL;
+		if(calledAlleles.length>0 && (calledAlleles.length>1 || calledAlleles[0]!=0)) {
+			outVariant.setVariantQS((short) 255);
+			byte type = GenomicVariant.TYPE_UNDETERMINED;
+			for(CalledGenomicVariant call:clusterCalls) {
+				type = GenomicVariantImpl.mergeType(type, call.getType());
+			}
+			if(type == GenomicVariant.TYPE_UNDETERMINED && first==last && finalAlleles.size()==2 && calledAlleles.length>0) type = GenomicVariant.TYPE_BIALLELIC_SNV;
+			outVariant.setType(type);
+			outCall.setGenotypeQuality((short)255);
+			format = VCFRecord.DEF_FORMAT_ARRAY_QUALITY;
+		}
+		
+		VCFRecord answer = new VCFRecord(outVariant, format, outCall, header);
+		return answer;
+	}
+	private String serializeCalls(List<CalledGenomicVariant> clusterCalls) {
+		StringBuilder out = new StringBuilder();
+		for(CalledGenomicVariant call:clusterCalls) {
+			out.append(call.getSampleId()+":"+call.getAlleles().length+".");
+		}
+		return out.toString();
+	}
+	private String makeCall(String sequenceName, int first, int last, String refAllele, List<CalledGenomicVariant> clusterCalls, String fileId) {
+		
+		List<CalledGenomicVariant> callsFile = new ArrayList<>();
+		for(CalledGenomicVariant call:clusterCalls) {
+			if(fileId.equals(call.getSampleId())) {
+				if(call.isUndecided() || call.isHeterozygous()) return null;
+				if(call.isHomozygousReference()) continue;
+				callsFile.add(call);
+			}
+		}
+		//if(first == posPrint) System.out.println("For "+sequenceName+":"+first+"-"+last+". File: "+fileId+" clusterCalls: "+callsFile.size());
+		if(callsFile.size()==0) return refAllele;
+		if(first == last) return callsFile.get(0).getCalledAlleles()[0];
+		StringBuilder outCall = new StringBuilder();
+		int nextPosCall=first;
+		for(CalledGenomicVariant call:callsFile) {
+			//if(first == posPrint) System.out.println("For "+sequenceName+":"+first+"-"+last+". File: "+fileId+" call allele: "+call.getCalledAlleles()[0]+" quality: "+call.getGenotypeQuality());
+			if(nextPosCall<call.getFirst()) {
+				CharSequence left = genome.getReference(sequenceName, nextPosCall, call.getFirst()-1);
+				if(left == null ) return null;
+				outCall.append(left);
+			} else if (nextPosCall>call.getFirst()) {
+				//Overlapping calls within a single file
+				return null;
+			}
+			outCall.append(call.getCalledAlleles()[0]);
+			nextPosCall = call.getLast()+1;
+		}
+		if(nextPosCall<=last) {
+			CharSequence right = genome.getReference(sequenceName, nextPosCall, last);
+			if(right == null ) return null;
+			outCall.append(right);
+		} else if (nextPosCall>last+1) {
+			//Calls go over the last position
+			return null;
+		}
+		if(first == posPrint) System.out.println("For "+sequenceName+":"+first+"-"+last+". File: "+fileId+" final call: "+outCall.toString());
+		return outCall.toString();
 	}
 }
