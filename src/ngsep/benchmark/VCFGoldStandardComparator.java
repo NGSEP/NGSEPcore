059864fd700559a10d34f9c7bf0bb04cb2582157
jorge <jorge@jorge-0420006391>
2018-03-10 17:37:59 -0500

    Improved comparison when gold standard has reference calls

--- src/ngsep/benchmark/VCFGoldStandardComparator.java
+++ src/ngsep/benchmark/VCFGoldStandardComparator.java
@@ -1,16 +1,19 @@
 package ngsep.benchmark;
 import java.io.IOException;
 import java.io.PrintStream;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Iterator;
+import java.util.List;
 import java.util.Map;
+import java.util.Set;
+import java.util.TreeSet;
 
+import ngsep.genome.GenomicRegionComparator;
 import ngsep.genome.ReferenceGenome;
 import ngsep.sequences.QualifiedSequenceList;
-import ngsep.variants.AlleleCompatibilityGenomicVariantComparator;
 import ngsep.variants.CalledGenomicVariant;
 import ngsep.variants.GenomicVariant;
-import ngsep.variants.SNV;
 import ngsep.vcf.VCFFileReader;
 import ngsep.vcf.VCFFileWriter;
 import ngsep.vcf.VCFRecord;
@@ -21,6 +24,7 @@ public class VCFGoldStandardComparator {
 	private String outVCF = null;
 	private int mode = 0;
 	private short minQuality = 0;
+	private int posPrint = 5068;
 
 	VCFFileWriter writer = new VCFFileWriter();
 	public static void main(String[] args) throws Exception {
@@ -40,102 +44,133 @@ public class VCFGoldStandardComparator {
 				
 	}
 
-	private void compareFiles(String vcfGS, String vcfTest) throws IOException {
-		AlleleCompatibilityGenomicVariantComparator comparator = new AlleleCompatibilityGenomicVariantComparator(genome);
+	public void compareFiles(String vcfGS, String vcfTest) throws IOException {
+		QualifiedSequenceList sequenceNames = genome.getSequencesMetadata();
+		GenomicRegionComparator compRegion = new GenomicRegionComparator(sequenceNames);
 		Map<Byte, GoldStandardComparisonCounts> countsPerType = new HashMap<>();
 		countsPerType.put(GenomicVariant.TYPE_BIALLELIC_SNV, new GoldStandardComparisonCounts());
 		countsPerType.put(GenomicVariant.TYPE_INDEL, new GoldStandardComparisonCounts());
 		countsPerType.put(GenomicVariant.TYPE_STR, new GoldStandardComparisonCounts());
 		//countsPerType.put(GenomicVariant.TYPE_UNDETERMINED, new GoldStandardComparisonCounts());
+		List<CalledGenomicVariant> callsTest = VCFFileReader.loadCalledVariantsSingleIndividualVCF(vcfTest);
 		int lastRowCounts = GoldStandardComparisonCounts.NUM_ROWS_COUNTS-1;
 		PrintStream out = null;
-		try (VCFFileReader inGS = new VCFFileReader(vcfGS);
-		     VCFFileReader inTest = new VCFFileReader(vcfTest);){ 
+		try (VCFFileReader inGS = new VCFFileReader(vcfGS)){ 
 			QualifiedSequenceList seqNames = genome.getSequencesMetadata();
 			inGS.setSequences(seqNames);
-			inTest.setSequences(seqNames);
-			
 			inGS.setLoadMode(VCFFileReader.LOAD_MODE_MINIMAL);
 			if(outVCF!=null) {
 				out = new PrintStream(outVCF);
 				writer.printHeader(inGS.getHeader(), out);
 			}
 			Iterator<VCFRecord> itGS = inGS.iterator();
-			Iterator<VCFRecord> itTest = inTest.iterator();
-			
-			VCFRecord rGS = null;
-			VCFRecord rTest = null;
-			if(itGS.hasNext()) rGS = itGS.next();
-			if(itTest.hasNext()) rTest = itTest.next();
-			while (rGS!=null && rTest!=null) {
+			int idxTest = 0;
+			while (itGS.hasNext()) {
+				VCFRecord rGS = itGS.next();
 				CalledGenomicVariant callGS = rGS.getCalls().get(0);
-				CalledGenomicVariant callTest = rTest.getCalls().get(0);
-				if(callGS.isUndecided() ) {
-					rGS = safeNext(itGS);
+				boolean referenceRegion = callGS.isHomozygousReference();
+				byte typeGS = loadType(callGS);
+				List<CalledGenomicVariant> callsIntersect = new ArrayList<>();
+				while(idxTest<callsTest.size()) {
+					CalledGenomicVariant callTest = callsTest.get(idxTest);
+					if(callTest.isUndecided()) {
+						idxTest++;
+						continue;
+					}
+					
+					byte type = loadType(callTest);
+					short qualTest = callTest.getGenotypeQuality();
+					int column = getGenotypeNumber(callTest);
+					int cmp = compRegion.compare(callGS, callTest);
+					if(posPrint==callGS.getFirst()) System.out.println("Call GS: "+callGS.getFirst()+" call test: "+callTest.getFirst()+" type: "+type+" column: "+column+" comparison: "+cmp);
+					if(cmp>1) {
+						column+=12;
+					}
+					if(cmp >1 || (cmp>=0 && referenceRegion)) {
+						countsPerType.get(type).update(0,Math.min(qualTest/10, lastRowCounts),column);
+						VCFRecord record = new VCFRecord(callTest, VCFRecord.DEF_FORMAT_ARRAY_QUALITY, callTest, null);
+						if(mode == 2 && qualTest>=minQuality && !callTest.isHomozygousReference()) writer.printVCFRecord(record, out);
+						idxTest++;
+						continue;
+					} else if (cmp<-1) break;
+					else if (cmp == -1){
+						if((callGS.isUndecided() || referenceRegion) && !callTest.isHomozygousReference()){
+							int overlap = callGS.getLast()-callTest.getFirst()+1;
+							int remainder = callTest.getLast() - callGS.getLast();
+							if(overlap < remainder) break;
+						}
+					}
+					if(!referenceRegion) {
+						callsIntersect.add(callTest);
+					} else {
+						countsPerType.get(type).update(0,Math.min(qualTest/10, lastRowCounts),column);
+					}
+					idxTest++;
+				}
+				if(posPrint==callGS.getFirst()) System.out.println("GS call at "+callGS.getSequenceName()+":"+callGS.getFirst()+" type: "+typeGS+" undecided: "+callGS.isUndecided()+" Calls intersect: "+callsIntersect.size()+" next test call: "+callsTest.get(idxTest).getFirst());
+				if(callGS.isUndecided() || referenceRegion ) {
+					//Ignore variants that intersect with unknown gold standard sites
 					continue;
 				}
-				if(callTest.isUndecided() ) {
-					rTest = safeNext(itTest);
-					continue;
-				}
-				int cmp = comparator.compare(rGS.getVariant(), rTest.getVariant());
-				byte typeGS = loadType(rGS);
-				byte typeTest = loadType(rTest);
-				
-				short qualTest = callTest.getGenotypeQuality();
+				//Process non-reference call in the gold standard
 				int n1 = getGenotypeNumber(callGS);
-				int n2 = getGenotypeNumber(callTest);
-				if(cmp<0) {
+				if(callsIntersect.size()==0) {
 					//Gold standard variant not found
 					countsPerType.get(typeGS).update(0,lastRowCounts,9+n1);
-					//if(typeGS==GenomicVariant.TYPE_INDEL) System.out.println("Printing variant");
-					if(mode == 1) writer.printVCFRecord(rGS, out); 
-					rGS = safeNext(itGS);
-				} else if (cmp>0) {
-					//False positive variant
-					countsPerType.get(typeTest).update(0,Math.min(qualTest/10, lastRowCounts),12+n2);
-					if(mode == 2 && qualTest>=minQuality) writer.printVCFRecord(rTest, out);
-					rTest = safeNext(itTest);
-				} else {
-					int k = 3*n1+n2;
-					int row = Math.min(qualTest/10, lastRowCounts);
-					GoldStandardComparisonCounts counts = countsPerType.get(typeGS); 
-					counts.update(0,row,k);
-					counts.update(row+1,lastRowCounts,9+n1);
-					if(mode == 1 &&  qualTest<minQuality) {
-						writer.printVCFRecord(rGS, out);
-						writer.printVCFRecord(rTest, out);
-					}
-					if(mode==3 && n1!=n2 &&  qualTest>=minQuality) {
-						writer.printVCFRecord(rGS, out);
-						writer.printVCFRecord(rTest, out);
-					}
-					rGS = safeNext(itGS);
-					rTest = safeNext(itTest);
-				}
-			}
-			while(rGS!=null) {
-				//Gold standard variant not found
-				CalledGenomicVariant callGS = rGS.getCalls().get(0);
-				if(!callGS.isUndecided()) {
-					byte typeGS = loadType(rGS);
-					int n1 = getGenotypeNumber(callGS);
-					countsPerType.get(typeGS).update(0,lastRowCounts,9+n1);
 					if(mode == 1) writer.printVCFRecord(rGS, out);
+					continue;
 				}
-				rGS = safeNext(itGS);
+				boolean covered = false;
+				for(CalledGenomicVariant callTest:callsIntersect) {
+					Set<String> allelesGS = buildAlleleStrings(callGS, callTest.getFirst(), callTest.getLast());
+					Set<String> allelesTest = buildAlleleStrings(callTest, callGS.getFirst(), callGS.getLast());
+					boolean consistent = allelesGS.equals(allelesTest); 
+					byte typeTest = loadType(callTest);
+					short qualTest = callTest.getGenotypeQuality();
+					int n2 = getGenotypeNumber(callTest);
+					VCFRecord rTest = new VCFRecord(callTest, VCFRecord.DEF_FORMAT_ARRAY_QUALITY, callTest, null);
+					if(consistent) {
+						GoldStandardComparisonCounts counts = countsPerType.get(typeGS);
+						int k = 3*n1+n2;
+						int row = Math.min(qualTest/10, lastRowCounts); 
+						counts.update(0,row,k);
+						counts.update(row+1,lastRowCounts,9+n1);
+						if(mode == 1 &&  qualTest<minQuality) {
+							writer.printVCFRecord(rGS, out);
+							writer.printVCFRecord(rTest, out);
+						}
+						if(mode==3 && n1!=n2 &&  qualTest>=minQuality) {
+							writer.printVCFRecord(rGS, out);
+							writer.printVCFRecord(rTest, out);
+						}
+					} else {
+						//False positive for inconsistent variant
+						countsPerType.get(typeTest).update(0,Math.min(qualTest/10, lastRowCounts),12+n2);
+						if(mode==3 && qualTest>=minQuality) {
+							writer.printVCFRecord(rGS, out);
+							writer.printVCFRecord(rTest, out);
+						}
+					}
+				}
+				if(!covered) {
+					GoldStandardComparisonCounts counts = countsPerType.get(typeGS);
+					//False negative for GS not properly called
+					counts.update(0,lastRowCounts,9+n1);
+					
+					
+				}
 			}
-			while(rTest!=null) {
-				//False positive variant
-				CalledGenomicVariant callTest = rTest.getCalls().get(0);
+			while(idxTest<callsTest.size()) {
+				//Process test variants at the end
+				CalledGenomicVariant callTest = callsTest.get(idxTest);
 				if(!callTest.isUndecided()) {
-					byte typeTest = loadType(rTest);
+					byte typeTest = loadType(callTest);
 					short qualTest = callTest.getGenotypeQuality();
 					int n2 = getGenotypeNumber(callTest);
 					countsPerType.get(typeTest).update(0,Math.min(qualTest/10, lastRowCounts),12+n2);
-					if(mode == 2 && qualTest>=minQuality) writer.printVCFRecord(rTest, out);
+					VCFRecord rTest = new VCFRecord(callTest, VCFRecord.DEF_FORMAT_ARRAY_QUALITY, callTest, null);
+					if(mode == 2 && qualTest>=minQuality && !callTest.isHomozygousReference()) writer.printVCFRecord(rTest, out);
 				}
-				rTest = safeNext(itTest);
 			}
 		} finally {
 			if(out!=null) {
@@ -150,24 +185,36 @@ public class VCFGoldStandardComparator {
 		System.out.println("STRs/OTHER");
 		countsPerType.get(GenomicVariant.TYPE_STR).print(System.out);
 	}
-
 	
-	private VCFRecord safeNext(Iterator<VCFRecord> itGS) {
-		VCFRecord rGS;
-		if(itGS.hasNext()) rGS = itGS.next();
-		else rGS = null;
-		return rGS;
+	private Set<String> buildAlleleStrings(GenomicVariant v, int firstRegion, int lastRegion) {
+		String seqName = v.getSequenceName();
+		String left = null;
+		if(firstRegion<v.getFirst()) left = genome.getReference(seqName, firstRegion, v.getFirst()-1).toString();
+		String right = null;
+		if(v.getLast()<lastRegion) right = genome.getReference(seqName, v.getLast()+1, lastRegion).toString();
+		Set<String> allelesSet = new TreeSet<>();
+		String [] alleles = v.getAlleles();
+		for(int i=0;i<alleles.length;i++) {
+			String allele = alleles[i];
+			int cut = v.getLast()-lastRegion-(alleles[0].length()-allele.length());
+			if(cut>0 && cut < allele.length()) allele = allele.substring(0, allele.length()-cut);
+			String allO = "";
+			if(left!=null) allO += left;
+			allO+=allele;
+			if(right!=null) allO += right;
+			allelesSet.add(allO.toUpperCase());
+		}
+		return allelesSet;
 	}
 	
-	private byte loadType(VCFRecord record) {
-		GenomicVariant v = record.getVariant();
-		byte type = v.getType();
+	private byte loadType(CalledGenomicVariant call) {
+		byte type = call.getType();
 		if(type == GenomicVariant.TYPE_BIALLELIC_SNV) return type;
 		if(type == GenomicVariant.TYPE_INDEL) return type;
 		if(type == GenomicVariant.TYPE_STR) return type;
-		if(v instanceof SNV) return GenomicVariant.TYPE_BIALLELIC_SNV;
-		String [] alleles = v.getAlleles();
+		String [] alleles = call.getAlleles();
 		if(alleles.length==2 && alleles[0].length()!=alleles[1].length()) return GenomicVariant.TYPE_INDEL;
+		else if(alleles.length==2 && alleles[0].length()==1) return GenomicVariant.TYPE_BIALLELIC_SNV;
 		return GenomicVariant.TYPE_STR;
 	}
 
