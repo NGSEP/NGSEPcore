7efe4e4b35eb49f150067a4054982b4068196396
danielt <danielt@DanielTUbuntu>
2018-05-04 09:44:34 -0500

    Implemented imput for LCS

--- src/ngsep/assembly/GenomesAligner.java
+++ src/ngsep/assembly/GenomesAligner.java
@@ -3,14 +3,22 @@ package ngsep.assembly;
 import java.io.IOException;
 import java.io.PrintStream;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.SortedSet;
+import java.util.TreeMap;
+import java.util.TreeSet;
 
 import ngsep.alignments.ReadAlignment;
 import ngsep.genome.GenomicRegion;
+import ngsep.genome.GenomicRegionPositionComparator;
+import ngsep.genome.GenomicRegionSortedCollection;
 import ngsep.genome.ReferenceGenome;
 import ngsep.main.CommandsDescriptor;
-import ngsep.math.Distribution;
-import ngsep.sequences.DNAMaskedSequence;
 import ngsep.sequences.FMIndex;
 import ngsep.sequences.QualifiedSequence;
 import ngsep.sequences.QualifiedSequenceList;
@@ -40,8 +48,7 @@ public class GenomesAligner {
 		String fileTranscriptome2 = args[i++];
 		String outPrefix = args[i++]; 
 		instance.loadGenomes(fileGenome1,fileTranscriptome1, fileGenome2, fileTranscriptome2);
-		instance.findUniqueCopyOrthologs();
-		instance.alignOrthologUnits();
+		instance.alignGenomes();
 		try (PrintStream outAlignmnent = new PrintStream(outPrefix+"_lcs.txt");) {
 			instance.printAlignmentOrthologUnits (outAlignmnent);
 		}
@@ -62,52 +69,197 @@ public class GenomesAligner {
 	}
 	
 	
-	public void findUniqueCopyOrthologs() {
+	public void alignGenomes() {
 		//Create orthology units based on transcripts
 		
 		List<OrthologyUnit> unitsT1 = extractOrthologyUnits(transcriptome1);
 		List<OrthologyUnit> unitsT2 = extractOrthologyUnits(transcriptome2);
-		QualifiedSequenceList proteinSequencesT1 = extractProteinSequences(unitsT1);
-		QualifiedSequenceList proteinSequencesT2 = extractProteinSequences(unitsT2);	
-
-		
-		//Build two FMIndex objects for proteins in the two genomes
-		
-		//FMIndex for qualified sequence list for genome 1
-		FMIndex FMIndex1 = new FMIndex();
-		FMIndex1.loadQualifiedSequenceList(proteinSequencesT1);
-		
-		
-		// FMIndex for qualified sequence list for genome 2
-		FMIndex FMIndex2 = new FMIndex();
-		FMIndex2.loadQualifiedSequenceList(proteinSequencesT2);
-		
-		
-		
-
 		
 		//Query each orthology unit against its own genome and select the units that are unique
-		QualifiedSequenceList uniquesUnitsT1 = searchUniqueUnits(FMIndex1, proteinSequencesT1);
-		System.out.println("Total proteins 1: "+proteinSequencesT1.size()+" Unique units T1: "+uniquesUnitsT1.size());
+		List<OrthologyUnit>  uniquesUnitsT1 = selectUniqueUnits(unitsT1);
+		System.out.println("Total units 1: "+unitsT1.size()+" Unique units T1: "+uniquesUnitsT1.size());
+		List<OrthologyUnit> uniquesUnitsT2 = selectUniqueUnits(unitsT2);
+		System.out.println("Total proteins 2: "+unitsT2.size()+" Unique units T2: "+uniquesUnitsT2.size());
 		
-		QualifiedSequenceList uniquesUnitsT2 = searchUniqueUnits(FMIndex2, proteinSequencesT2);
-		System.out.println("Total proteins 2: "+proteinSequencesT2.size()+" Unique units T2: "+uniquesUnitsT2.size());
+		QualifiedSequenceList genomeMetadata1 = genome1.getSequencesMetadata();
+		GenomicRegionSortedCollection<OrthologyUnit> unitsC1 = new GenomicRegionSortedCollection<>(genomeMetadata1);
+		unitsC1.addAll(uniquesUnitsT1);
+		QualifiedSequenceList genomeMetadata2 = genome2.getSequencesMetadata();
+		GenomicRegionSortedCollection<OrthologyUnit> unitsCollectionG2 = new GenomicRegionSortedCollection<>(genomeMetadata2);
+		unitsCollectionG2.addAll(uniquesUnitsT2);
+		
+		for(QualifiedSequence chrG1:genomeMetadata1) {
+			List<OrthologyUnit> unitsChrG1 = unitsC1.getSequenceRegions(chrG1.getName()).asList();
+			fillMateIds(unitsChrG1,unitsCollectionG2);
+			String chrNameG2 = findBestChromosome(unitsChrG1,unitsCollectionG2);
+			QualifiedSequence chrG2 = genomeMetadata2.get(chrNameG2);
+			List<OrthologyUnit> unitsChrG2 = unitsCollectionG2.getSequenceRegions(chrG2.getName()).asList();
+			List<OrthologyUnit> selectedUnits = alignOrthologyUnits(unitsChrG1,unitsChrG2);
+			System.out.println("Chr g1: "+chrG1.getName()+" chrG2: "+chrG2.getName()+" LCS size: "+selectedUnits.size());
+		}
 		
 		
 		
-		//Query selected units against the other genome to assign mates
-		QualifiedSequenceList mates1 = searchUniqueUnits(FMIndex1, uniquesUnitsT2);
-		System.out.println("Mates T1: "+mates1.size());
-		QualifiedSequenceList mates2 = searchUniqueUnits(FMIndex2, uniquesUnitsT1);
-		System.out.println("Mates T2: "+mates2.size());
+
+		
 	}
 	
+	/**
+	 * FInds the unique mate in the second genome of the units in the first genome
+	 * @param unitsG1
+	 * @param unitsG2
+	 */
+	private void fillMateIds(List<OrthologyUnit> unitsG1, GenomicRegionSortedCollection<OrthologyUnit> unitsG2) {
+		//Build FMindex g2
+		
+		QualifiedSequenceList proteinSequencesG2 = extractProteinSequences(unitsG2.asList());
+		FMIndex indexG2 = new FMIndex();
+		indexG2.loadQualifiedSequenceList(proteinSequencesG2);
+		//Search proteins from G1 using method findSimilarProteins
+		
+		for(int i=0; i<unitsG1.size(); i++)
+		{
+			OrthologyUnit unit = unitsG1.get(i);
+			String protein = unit.getProteinSequence();
+			Set<String> hits = findSimilarProteins(indexG2, protein);
+		//If the hit is unique fill the mate information using setMateId from OrthologyUnit
+			if(hits.size() == 1)
+			{
+				Iterator<String> iterator = hits.iterator();
+				String mateId = iterator.next();
+				unit.setMateId(mateId);
+			}
+		}
+	}
+		
 
+
+
+	/**
+	 * Selects the chromosome in unitsC2 having the largest number of mates with the given units from genome 1
+	 * @param unitsG1
+	 * @param unitsC2
+	 * @return String name of the chromosome in the second genome
+	 */
+	private String findBestChromosome(List<OrthologyUnit> unitsG1, GenomicRegionSortedCollection<OrthologyUnit> unitsG2) {
+		Map<String,OrthologyUnit> unitsC2Map = new HashMap<>();
+		for(OrthologyUnit unit:unitsG2) unitsC2Map.put(unit.getId(), unit);
+		
+		Map<String,Integer> chrG2Counts = new HashMap<>();
+		
+
+		//Go over the orthology units of G1. Locate chromosome of each mate and update counts map
+		
+		for(int i=0; i<unitsG1.size(); i++)
+		{
+			String mateId = unitsG1.get(i).getMateId();
+			if(mateId==null) continue;
+			OrthologyUnit unitG2 = unitsC2Map.get(mateId);
+			if(unitG2 == null) continue;
+			//Recupero la unidad de ortología del mapa unitsC2Map
+			//Recupero el cromosoma
+			String sequenceName = unitG2.getSequenceName();			
+			if((chrG2Counts.containsKey(sequenceName)))
+			{
+				int value = chrG2Counts.get(sequenceName)+1;
+				chrG2Counts.put(sequenceName, value);
+			}
+			else
+			{
+				chrG2Counts.put(sequenceName, 1);
+			}
+				
+		}
+		//Find the chromosome with the largest count
+		int bestChromosomeCounts = 0;
+		String bestChromosome = null;
+		
+		for(Map.Entry<String,Integer> entry : chrG2Counts.entrySet()) {
+			String name = entry.getKey();
+			int chromosomeProteins = entry.getValue();
+			
+			if(chromosomeProteins > bestChromosomeCounts)
+			{
+				bestChromosomeCounts = chromosomeProteins;
+				bestChromosome = name;
+			}
+		}
+
+		return bestChromosome;
+	}
+
+
+
+
+	/**
+	 * Aligns the orthology units from two homologous chromosomes using LCS
+	 * @param unitsChrG1 This list has only one chromosome and is sorted by position
+	 * @param unitsChrG2 This list has only one chromosome and is sorted by position
+	 * @return List<OrthologyUnit> List of selected units of the first list making the LCS relative to the second list
+	 */
+	private List<OrthologyUnit> alignOrthologyUnits(List<OrthologyUnit> unitsChrG1, List<OrthologyUnit> unitsChrG2) {
+		Map<String,OrthologyUnit> unitsC2Map = new HashMap<>();
+		for(OrthologyUnit unit:unitsChrG2) unitsC2Map.put(unit.getId(), unit);
+		List<OrthologyUnit> answer = new ArrayList<>();
+		
+		List<OrthologyUnit> unitsG1List = new ArrayList<>();
+		List<OrthologyUnit> unitsG2List = new ArrayList<>();
+		//Assigns mates of units in G2
+		
+		// Select orthology units in G1 having mate in g2 and assign the mate of units in G2.
 	
-	public QualifiedSequenceList searchUniqueUnits (FMIndex fmindex, QualifiedSequenceList proteinSequences)
+		//At the same time create two new lists of the same size with the  units in g1 having its mate in g2
+		
+		for(int i=0; i<unitsChrG1.size(); i++)
+		{
+			OrthologyUnit unitG1 = unitsChrG1.get(i);
+			String mateId = unitG1.getMateId();
+			if(mateId==null) continue;
+			OrthologyUnit unitG2 = unitsC2Map.get(mateId);
+			if(unitG2 == null) continue;
+			unitsG1List.add(unitG1);			
+			unitsG2List.add(unitG2);
+			unitG2.setMateId(unitG1.getId());
+		}
+		Collections.sort(unitsG2List, GenomicRegionPositionComparator.getInstance());		
+		
+		// Create int array with the positions in g2 for the units in g1. Input for LCS
+		Map<String,Integer> sortedUnitsG2Pos = new HashMap<>();
+		for(int i=0; i<unitsG2List.size(); i++) {
+			OrthologyUnit unit=unitsG2List.get(i);
+			sortedUnitsG2Pos.put(unit.getId(), i);
+		}
+		int []positions = new int[unitsG1List.size()];
+		for(int i=0; i<unitsChrG1.size(); i++)
+		{
+			OrthologyUnit unitG1 = unitsChrG1.get(i);
+			int j = sortedUnitsG2Pos.get(unitG1.getMateId());
+			positions[i] = j;
+		}
+		// Run LCS
+		
+		Set<Integer> lcs = findLCS(positions);
+		// Select the orthology units in G1 located at the indexes given by the output of LCS
+		for(int i:lcs)
+		{
+			OrthologyUnit lcsResult = unitsG1List.get(i);
+			answer.add(lcsResult);
+		}
+		
+		
+		return answer;
+	}
+
+
+
+
+	public List<OrthologyUnit> selectUniqueUnits (List<OrthologyUnit> units)
 	{
 	
-		QualifiedSequenceList uniques = new QualifiedSequenceList();
+		List<OrthologyUnit> uniques = new ArrayList<>();
+		QualifiedSequenceList proteinSequences = extractProteinSequences(units);
+		FMIndex index = new FMIndex();
+		index.loadQualifiedSequenceList(proteinSequences);
 		
 		for (int i=0; i<proteinSequences.size(); i++)
 		{
@@ -115,16 +267,71 @@ public class GenomesAligner {
 			String nextSequence = next.getCharacters().toString();
 			
 			
-			List<ReadAlignment> searchResult = fmindex.search(nextSequence);
-			if(searchResult.size() == 1)
+			Set<String> hits = findSimilarProteins(index, nextSequence);
+			if(hits.size() == 1)
 			{
-				uniques.add(next);
+				uniques.add(units.get(i));
 			}
 		}
 		
 		return uniques;
 	}
+	
+	private static Set<String> findSimilarProteins(FMIndex index, String protein1) {
+		
+		
+		// Reemplazar estas 3 lineas de código por:
+		// Ver nota de voz
+		// Buscar K-mers de la proteína (subcadenas)
+		// a cada substring se le hace un FMindex y se le hace un search
+		//List<ReadAlignment> alns = index.search(protein1);
+		//for(ReadAlignment aln:alns) {
+		//	answer.add(aln.getSequenceName());
+		//}
+		
+		//el mapa es transcript y cuantos kmers lo soporta
+		Map<String,Integer> kmerSupportMap = new TreeMap<>();
+		int totalKmers = 0;
+		int kmerSize = 10;
+		//PASO 1: Generar n kmers y por cada uno consultar en el fm index los transcripts que tienen el kmer para llenar el mapa
+		
+		for(int i=0; i<protein1.length()-kmerSize+1; i++) {
+			String kmer = protein1.substring(i, i+kmerSize);
+			
+			List <ReadAlignment> kmerHits = index.search(kmer);
+			for(ReadAlignment alns:kmerHits) {
+				String name = alns.getSequenceName();
+				if(kmerSupportMap.containsKey(name))
+				{
+					int value = 1 + kmerSupportMap.get(name);
+					kmerSupportMap.put(name, value);
+				}
+				else
+				{
+					kmerSupportMap.put(name, 1);
+				}
+			}
+			totalKmers = totalKmers +1;
+		}
 
+		
+		//PASO 2: Llenar la lista de respuesta recorriendo el mapa y escogiendo los ids de transcripts que
+		//para los que al menos el 80% de totalKmers dice que el transcript es paralogo de protein1
+		Set<String> answer = new TreeSet<>();
+		
+		for(Map.Entry<String,Integer> entry : kmerSupportMap.entrySet()) {
+			String name = entry.getKey();
+			double transcriptKmers = entry.getValue();
+			double percent = (transcriptKmers/totalKmers)*100;
+			if(percent >= 80)
+			{
+				answer.add(name);
+			}
+			
+		}
+		
+		return answer;
+	}
 	
 
 	/**
@@ -217,14 +424,23 @@ public class GenomesAligner {
 		
 		return bestTranscript;
 	}
-//probar esto y todos los metodos que haga 
-	public void alignOrthologUnits() {
-		// TODO: Implement LCS algorithm to align ortholog units
+	
+	public SortedSet<Integer> findLCS (int [] indexesMap) {
+		SortedSet<Integer> answer = new TreeSet<>();
+		int n = indexesMap.length;
+		int [] [] m = new int [n][n+1];
+		//Llenar la matriz m de acuerdo con la ecuacion de recurrencia
 		
+		
+		
+		//El valor optimo de indices a escoger esta en m[n-1][n]
+		//Para calcular los indices optimos es necesario hacer el camino de vuelta recordando las decisiones que llevan al optimo
+		//COmo en el algoritmo para alinear secuencias
+		
+		return answer;
 	}
-
-
-
+	
+	
 	private void printAlignmentOrthologUnits(PrintStream outAlignmnent) {
 		// TODO Auto-generated method stub
 		
