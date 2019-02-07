14183521faaf2c831bb7c3b2aca158403a110df0
jc.bojaca <jc.bojaca@ISC435.sis.virtual.uniandes.edu.co>
2019-02-07 13:50:21 -0500

    copy the others methods and finish the maping (quee and treeK only in one strand)

--- src/ngsep/assembly/GraphBuilder_OverlapFinder_Tree.java
+++ src/ngsep/assembly/GraphBuilder_OverlapFinder_Tree.java
@@ -16,10 +16,11 @@ import java.util.TreeMap;
 import ngsep.alignments.ReadAlignment;
 import ngsep.sequences.FMIndex;
 
-public class GraphBuilder_OverlapFinder_Tree implements GraphBuilder_OverlapFinder {
+public class GraphBuilder_OverlapFinder_Tree implements
+		GraphBuilder_OverlapFinder {
 	private final static double Rate_of_changes = 0.07;
 	private final static double Rate_of_cuts = 0.03;
-	private final static double Rate_of_cover = 1;
+	private final static double Rate_of_cover = 0.5;
 	public static final double permitedBorderRate = 0.15;
 
 	private List<CharSequence> sequences;
@@ -33,7 +34,8 @@ public class GraphBuilder_OverlapFinder_Tree implements GraphBuilder_OverlapFind
 
 	@Override
 	public void calculate(List<CharSequence> seq, FMIndex index) {
-		this.kmerIterator = new GraphBuilder_KmerIterator(Rate_of_changes, Rate_of_cuts, Rate_of_cover);
+		this.kmerIterator = new GraphBuilder_KmerIterator(Rate_of_changes,
+				Rate_of_cuts, Rate_of_cover);
 		this.index = index;
 		this.sequences = seq;
 		findOverlaps();
@@ -52,16 +54,19 @@ public class GraphBuilder_OverlapFinder_Tree implements GraphBuilder_OverlapFind
 				continue;
 
 			alignments.clear();
-			findAlignments(seqId, kmerIterator.positiveStrand(sequences.get(seqId)));
+			findAlignments(seqId,
+					kmerIterator.positiveStrand(sequences.get(seqId)));
 			detectOverlap(seqId, false);
 
 			alignments.clear();
-			findAlignments(seqId, kmerIterator.negativeStrand(sequences.get(seqId)));
+			findAlignments(seqId,
+					kmerIterator.negativeStrand(sequences.get(seqId)));
 			detectOverlap(seqId, true);
 		}
 	}
 
-	private void findAlignments(int id_Ref, Iterable<Entry<Integer, String>> kmerIters) {
+	private void findAlignments(int id_Ref,
+			Iterable<Entry<Integer, String>> kmerIters) {
 		for (Entry<Integer, String> entry : kmerIters) {
 			int pos_Ref = entry.getKey();
 			for (ReadAlignment readAlignment : index.search(entry.getValue())) {
@@ -76,25 +81,29 @@ public class GraphBuilder_OverlapFinder_Tree implements GraphBuilder_OverlapFind
 	private void addAlingments(int id_Ref, int pos_Ref, int id_Lec, int pos_Lec) {
 		if (embeddedOverlaps.containsKey(id_Lec))
 			return;
-		TreeMap<Integer, Alignment> treeMap = alignments.computeIfAbsent(id_Lec,
-				key -> new TreeMap<Integer, Alignment>());
+		TreeMap<Integer, Alignment> treeMap = alignments.computeIfAbsent(
+				id_Lec, key -> new TreeMap<Integer, Alignment>());
 
 		int key = pos_Lec - pos_Ref;
 		Alignment aln = aling(treeMap, key);
 
 		if (aln != null) {
-			aln.setLengthLec(kmerIterator.SEARCH_KMER_LENGTH + (pos_Lec - aln.getPosLec()));
-			aln.setLengthRef(kmerIterator.SEARCH_KMER_LENGTH + (pos_Ref - aln.getPosRef()));
+			aln.setLengthLec(kmerIterator.SEARCH_KMER_LENGTH
+					+ (pos_Lec - aln.getPosLec()));
+			aln.setLengthRef(kmerIterator.SEARCH_KMER_LENGTH
+					+ (pos_Ref - aln.getPosRef()));
 			treeMap.put(key, aln);
 		} else
-			treeMap.put(key,
-					new Alignment(pos_Ref, pos_Lec, kmerIterator.SEARCH_KMER_LENGTH, kmerIterator.SEARCH_KMER_LENGTH));
+			treeMap.put(key, new Alignment(pos_Ref, pos_Lec,
+					kmerIterator.SEARCH_KMER_LENGTH,
+					kmerIterator.SEARCH_KMER_LENGTH));
 	}
 
 	private Alignment aling(TreeMap<Integer, Alignment> treeMap, int key) {
 		int removeKey = key;
 		int min = kmerIterator.MAX_KMER_DES;
-		Integer celingKey = treeMap.ceilingKey(key), floorKey = treeMap.floorKey(key);
+		Integer celingKey = treeMap.ceilingKey(key), floorKey = treeMap
+				.floorKey(key);
 
 		if (celingKey != null && celingKey - key < min) {
 			min = celingKey - key;
@@ -104,56 +113,67 @@ public class GraphBuilder_OverlapFinder_Tree implements GraphBuilder_OverlapFind
 			min = key - floorKey;
 			removeKey = floorKey;
 		}
-		return (min != kmerIterator.MAX_KMER_DES) ? treeMap.remove(removeKey) : null;
+		return (min != kmerIterator.MAX_KMER_DES) ? treeMap.remove(removeKey)
+				: null;
 	}
 
 	private void detectOverlap(int id_Ref, boolean isReverse) {
 		int lenghtRef = sequences.get(id_Ref).length();
 		int Diff = kmerIterator.MAX_KMER_DES;
-		OtherSequence: for (Entry<Integer, TreeMap<Integer, Alignment>> entry : alignments.entrySet()) {
+		OtherSequence: for (Entry<Integer, TreeMap<Integer, Alignment>> entry : alignments
+				.entrySet()) {
 			int id_Lec = entry.getKey();
 			int lenghtLec = sequences.get(id_Lec).length();
 
 			int embbedLimit = lenghtLec - lenghtRef;
 			TreeMap<Integer, Alignment> tree = entry.getValue();
 
-			for (Alignment aln : tree.subMap(embbedLimit - Diff, true, 0 + Diff, true).values()) {
+			for (Alignment aln : tree.subMap(embbedLimit - Diff, true,
+					0 + Diff, true).values()) {
 				int pos_Lec = aln.getPosLec() - aln.getPosRef();
 				if (pos_Lec > 0 || pos_Lec < embbedLimit)
 					continue;
 				if (aln.getPosLec() > lenghtLec * permitedBorderRate
-						|| aln.getPosLec() + aln.getLengthLec() < lenghtLec * (1 - permitedBorderRate))
+						|| aln.getPosLec() + aln.getLengthLec() < lenghtLec
+								* (1 - permitedBorderRate))
 					continue;
 
 				// lec is embedded in ref.
-				embeddedOverlaps.put(id_Lec, new Embedded(id_Ref, -pos_Lec, isReverse));
+				embeddedOverlaps.put(id_Lec, new Embedded(id_Ref, -pos_Lec,
+						isReverse));
 				continue OtherSequence;
 			}
 
-			for (Alignment aln : tree.subMap(0 - Diff, true, Integer.MAX_VALUE, true).values()) {
+			for (Alignment aln : tree.subMap(0 - Diff, true, Integer.MAX_VALUE,
+					true).values()) {
 				int pos_Lec = aln.getPosLec() - aln.getPosRef();
 				if (pos_Lec < 0)
 					continue;
 				if (aln.getPosRef() > lenghtRef * permitedBorderRate
-						|| aln.getPosLec() + aln.getLengthLec() < lenghtLec * (1 - permitedBorderRate))
+						|| aln.getPosLec() + aln.getLengthLec() < lenghtLec
+								* (1 - permitedBorderRate))
 					continue;
 
 				// lec -> ref || lec -> ref'
-				overlaps.add(new Overlap(id_Lec, false, id_Ref, isReverse, lenghtLec - pos_Lec));
+				overlaps.add(new Overlap(id_Lec, false, id_Ref, isReverse,
+						lenghtLec - pos_Lec));
 				break;
 			}
 
-			for (Alignment aln : tree.descendingMap().subMap(embbedLimit + Diff, true, Integer.MIN_VALUE, true)
+			for (Alignment aln : tree.descendingMap()
+					.subMap(embbedLimit + Diff, true, Integer.MIN_VALUE, true)
 					.values()) {
 				int pos_Lec = aln.getPosLec() - aln.getPosRef();
 				if (pos_Lec > embbedLimit)
 					continue;
 				if (aln.getPosLec() > lenghtLec * permitedBorderRate
-						|| aln.getPosRef() + aln.getLengthRef() < lenghtRef * (1 - permitedBorderRate))
+						|| aln.getPosRef() + aln.getLengthRef() < lenghtRef
+								* (1 - permitedBorderRate))
 					continue;
 
 				// ref -> lec || ref' -> lec
-				overlaps.add(new Overlap(id_Ref, isReverse, id_Lec, false, lenghtRef + pos_Lec));
+				overlaps.add(new Overlap(id_Ref, isReverse, id_Lec, false,
+						lenghtRef + pos_Lec));
 				break;
 			}
 		}
@@ -162,7 +182,8 @@ public class GraphBuilder_OverlapFinder_Tree implements GraphBuilder_OverlapFind
 	private AssemblyGraph buidAssemblyGraph() {
 		int[] map = new int[sequences.size()];
 		Arrays.fill(map, 1);
-		Queue<Integer> queue = new PriorityQueue<Integer>((Integer a, Integer b) -> a - b);
+		Queue<Integer> queue = new PriorityQueue<Integer>(
+				(Integer a, Integer b) -> a - b);
 		for (int i : embeddedOverlaps.keySet()) {
 			queue.add(i);
 			map[i] = 0;
@@ -198,7 +219,8 @@ public class GraphBuilder_OverlapFinder_Tree implements GraphBuilder_OverlapFind
 			int parentId = emb.getIdSequence();
 			int parentPos = emb.getPos();
 			boolean isReverse = emb.isReversed();
-			AssembyEmbedded embedded = new AssembyEmbedded(sequences.get(embeddedId), parentPos, isReverse);
+			AssembyEmbedded embedded = new AssembyEmbedded(
+					sequences.get(embeddedId), parentPos, isReverse);
 			assemblyGraph.addEmbedded(map[parentId], embedded);
 		}
 
@@ -206,12 +228,18 @@ public class GraphBuilder_OverlapFinder_Tree implements GraphBuilder_OverlapFind
 		for (Overlap overlap : overlaps) {
 			if (!embeddedOverlaps.containsKey(overlap.getIdFrom())
 					&& !embeddedOverlaps.containsKey(overlap.getIdTo())) {
-				// TODO: finish the map of formats
+				boolean startFrom = !overlap.isFromReversed();
+				boolean startTo = startFrom ^ !overlap.isFromReversed();
+				AssemblyVertex from = assemblyGraph.getVertex(
+						map[overlap.getIdFrom()], startFrom);
+				AssemblyVertex to = assemblyGraph.getVertex(
+						map[overlap.getIdTo()], startTo);
+				assemblyGraph.addEdge(to, from, overlap.getLength());
 				count++;
 			}
 		}
 
-		System.out.println("		Overlaps: " + count);
+		System.out.println("		Overlaps: " + count + "/" + overlaps.size());
 
 		return assemblyGraph;
 	}
@@ -270,7 +298,8 @@ class Overlap {
 	private boolean toReversed;
 	private int length;
 
-	public Overlap(int idFrom, boolean fromReversed, int idTo, boolean toReversed, int length) {
+	public Overlap(int idFrom, boolean fromReversed, int idTo,
+			boolean toReversed, int length) {
 		this.idFrom = idFrom;
 		this.fromReversed = fromReversed;
 		this.idTo = idTo;
