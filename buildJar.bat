b21da3c23c67e0dca4df91eadc56573089f45fa9
jorge <jorge@jorge-0420006391>
2019-04-05 13:19:22 -0500

    Created bat script to build the jar on windows systems

---
+++ buildJar.bat
@@ -0,0 +1,10 @@
+mkdir dist
+jar -xf lib/jsci-core.jar JSci
+move JSci dist/
+jar -xf lib/htsjdk-1.129.jar htsjdk
+move htsjdk dist/
+xcopy /E bin dist
+jar -cfe NGSEPcore_3.3.2.jar ngsep.NGSEPcore -C dist . 
+rmdir /s /q dist
+:END
+cmd /k
