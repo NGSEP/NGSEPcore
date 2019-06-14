bf23e02ca852b011c76f6a5ffdb3f58b38b9a40f
jorge <jorge@jorge-0420006391>
2019-06-14 18:12:58 -0500

    Improved output alignment

--- src/ngsep/transcriptome/CodonCDSPairwiseAlignment.java
+++ src/ngsep/transcriptome/CodonCDSPairwiseAlignment.java
@@ -6,18 +6,21 @@ public class CodonCDSPairwiseAlignment {
 		int proteinLengthCDS2=cds2.length()/3;
 		int[][] scores = new int[proteinLengthCDS1+1][proteinLengthCDS2+1];
 		byte [][] direction = new byte [proteinLengthCDS1+1][proteinLengthCDS2+1];
-		int indelDistance = 2;
-		int mismatchDistance = 1;
+		int indelPenalty = -2;
+		int mismatchPenalty = -1;
+		int matchScore = 1;
 		for (int i = 0; i < scores.length; i++) {	
 			for (int j = 0; j < scores[i].length; j++) {
 				if(i==0 && j==0) {
 					scores[i][j]=0;
 					direction[i][j] = 0;
 				} else if (i==0) {
-					scores[i][j]=scores[i][j-1]+indelDistance;
+					//scores[i][j]=scores[i][j-1]+indelPenalty;
+					scores[i][j]=0;
 					direction[i][j] = 1;
 				} else if (j==0) {
-					scores[i][j]=scores[i-1][j]+indelDistance;
+					//scores[i][j]=scores[i-1][j]+indelPenalty;
+					scores[i][j]=0;
 					direction[i][j] = 2;
 				} else {
 					scores[i][j]=scores[i-1][j-1];
@@ -25,13 +28,14 @@ public class CodonCDSPairwiseAlignment {
 					String codon1 = cds1.substring(3*(i-1), 3*i);
 					String codon2 = cds2.substring(3*(j-1), 3*j);
 					boolean equal = codon1.equals(codon2);
-					if(!equal) scores[i][j]+=mismatchDistance;
-					if(scores[i][j-1]+indelDistance < scores[i][j]) {
-						scores[i][j] = scores[i][j-1]+indelDistance;
+					if(equal) scores[i][j]+=matchScore;
+					else scores[i][j]+=mismatchPenalty;
+					if(scores[i][j-1]+indelPenalty > scores[i][j]) {
+						scores[i][j] = scores[i][j-1]+indelPenalty;
 						direction[i][j] = 1;
 					}
-					if(scores[i-1][j]+indelDistance < scores[i][j]) {
-						scores[i][j] = scores[i-1][j]+indelDistance;
+					if(scores[i-1][j]+indelPenalty > scores[i][j]) {
+						scores[i][j] = scores[i-1][j]+indelPenalty;
 						direction[i][j] = 2;
 					}
 				}
@@ -39,11 +43,27 @@ public class CodonCDSPairwiseAlignment {
 		}
 		StringBuilder aln1 = new StringBuilder();
 		StringBuilder aln2 = new StringBuilder();
-		int i = scores.length-1;
-		int j = scores[i].length-1;
+		int maxI = scores.length-1;
+		int maxJ = scores[maxI].length-1;
+		int score = scores[maxI][maxJ];
+		for(int i=maxI-1;i>=0.5*scores.length;i--) {
+			if(scores[i][maxJ]>score) {
+				maxI = i;
+				score = scores[i][maxJ];
+			}
+		}
+		for(int j=maxJ-1;j>=0.5*scores[maxI].length;j--) {
+			if(scores[scores.length-1][j]>score) {
+				maxI = scores.length-1;
+				maxJ = j;
+				score = scores[maxI][maxJ];
+			}
+		}
+		
 		String gapCodon = "---";
 		
-		
+		int i= maxI;
+		int j= maxJ;
 		while(i>0 || j>0) {
 			byte d = direction[i][j];
 			if(d==0) {
