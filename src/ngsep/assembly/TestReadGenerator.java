f9391a7534aac23cdd063a82936d06285e325fd6
jc.bojaca <jc.bojaca@LAPTOP-V19LF2JN>
2019-07-15 00:38:01 -0500

    add Reference lect

--- src/ngsep/assembly/TestReadGenerator.java
+++ src/ngsep/assembly/TestReadGenerator.java
@@ -4,6 +4,7 @@ import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.PrintStream;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Random;
 import java.util.TreeSet;
@@ -17,29 +18,27 @@ public class TestReadGenerator {
 	private final static double Rate_of_cuts = 0.03;
 	private static Random rnd = new Random();
 
-	public TestReadGenerator(String path, String pathlect, int numberOfSequences, int[] sequencesdist,
-			int numberOfreads, int[] distribution, double... tasas) throws FileNotFoundException {
-		double rateChages = (tasas.length > 0) ? tasas[0] : Rate_of_changes;
-		double rateCuts = (tasas.length > 1) ? tasas[1] : Rate_of_cuts;
+	public TestReadGenerator(String path, String pathlect, String pathLecRef, int numberOfSequences, int[] odist,
+			int numberOfreads, int[] dist, double rateChanges, double rateIndels) throws FileNotFoundException {
 
-		System.out.println(rateChages);
-		System.out.println(rateCuts);
-		String[] ref = getRef(numberOfSequences, sequencesdist);
+		String[] ref = getRef(numberOfSequences, odist);
 		String[] lects = new String[numberOfreads];
+		String[] lectsRef = new String[numberOfreads];
 
-		for (int i = 0; i < lects.length; i++)
-			lects[i] = getLect(ref, distribution, rateCuts, rateChages);
+		for (int j = 0; j < lects.length; j++) {
+			int i = rnd.nextInt(ref.length);
+			int length = (int) (dist[0] + ((rnd.nextBoolean()) ? 1 : -1) * dist[1] * rnd.nextDouble());
+			int pos = rnd.nextInt(ref[i].length() - length);
+			lects[j] = getLect(ref[i], pos, length, rateIndels, rateChanges);
+			lectsRef[j] = ref[i].substring(pos, pos + length);
+		}
 
 		print(path, ref, "ref");
 		print(pathlect, lects, "lect");
+		print(pathLecRef, lectsRef, "lectRef");
 	}
 
-	private static String getLect(String[] ref, int[] dist, double Ncuts, double Nchng) {
-		int i = rnd.nextInt(ref.length);
-		int length = (int) (dist[0] + ((rnd.nextBoolean()) ? 1 : -1) * dist[1] * rnd.nextDouble());
-		int pos = rnd.nextInt(ref[i].length() - length);
-		String str = ref[i];
-
+	private static String getLect(String str, int pos, int length, double Ncuts, double Nchng) {
 		StringBuilder strb = new StringBuilder();
 		int j = 0;
 		for (int x : indexTo(length, (int) (length * Ncuts))) {
@@ -89,11 +88,10 @@ public class TestReadGenerator {
 	}
 
 	private static String createSequence(int length) {
-		StringBuilder str = new StringBuilder();
-		for (int i = 0; i < length; i++) {
-			str.append(next());
-		}
-		return str.toString();
+		char[] str = new char[length];
+		for (int i = 0; i < length; i++)
+			str[i] = next();
+		return new String(str);
 	}
 
 	private static char next() {
@@ -104,8 +102,11 @@ public class TestReadGenerator {
 		FastaSequencesHandler handler = new FastaSequencesHandler();
 		List<QualifiedSequence> list = new ArrayList<QualifiedSequence>();
 		int i = 1;
-		for (String str : sec)
+		for (String str : sec) {
+			if (str == null)
+				System.out.println(i);
 			list.add(new QualifiedSequence(name + "_" + (i++), str));
+		}
 		try (PrintStream pr = new PrintStream(new FileOutputStream(file))) {
 			handler.saveSequences(list, pr, 1000);
 		}
@@ -113,32 +114,49 @@ public class TestReadGenerator {
 
 	public static void main(String[] args) throws Exception {
 		if (args.length < 8)
-			throw new Exception("faltan parametros");
+			throw new Exception("Faltan Parametros para generar los archivos");
 
 		String path = args[0];
 		String pathlect = args[1];
+		String pathLecRef = addToPathName(pathlect, "_ref");
 		int numberOfSequences = Integer.parseInt(args[2]);
 		int[] odist = new int[] { Integer.parseInt(args[3]), Integer.parseInt(args[4]) };
 		int numberOfreads = Integer.parseInt(args[5]);
 		int[] dist = new int[] { Integer.parseInt(args[6]), Integer.parseInt(args[7]) };
+		double rateChanges = (args.length > 8) ? Double.parseDouble(args[8]) : Rate_of_changes;
+		double rateIndels = (args.length > 9) ? Double.parseDouble(args[9]) : Rate_of_cuts;
+		printInfo(path, pathlect, pathLecRef, numberOfSequences, odist, numberOfreads, dist, rateChanges, rateIndels);
 
-		switch (args.length) {
-		case 8:
-			new TestReadGenerator(path, pathlect, numberOfSequences, odist, numberOfreads, dist);
-			break;
+		new TestReadGenerator(path, pathlect, pathLecRef, numberOfSequences, odist, numberOfreads, dist, rateChanges,
+				rateIndels);
+	}
 
-		case 9:
-			new TestReadGenerator(path, pathlect, numberOfSequences, odist, numberOfreads, dist,
-					Double.parseDouble(args[8]));
-			break;
+	private static void printInfo(String path, String pathlect, String pathLecRef, int numberOfSequences, int[] odist,
+			int numberOfreads, int[] dist, double rateChanges, double rateIndels) {
+		System.out.println("Ruta archivo de referencia: " + path);
+		System.out.println("Ruta lecturas de referencia: " + pathLecRef);
+		System.out.println("Ruta levturas: " + pathlect);
+		System.out.println("Referencia");
+		System.out.println("Secuencias:" + numberOfSequences + "   ~Uniforme(" + (odist[0] - odist[1]) + ", "
+				+ (odist[0] + odist[1]) + ")");
+		System.out.println("Lecturas");
+		System.out.println("Secuencias:" + numberOfreads + "   ~Uniforme(" + (dist[0] - dist[1]) + ", "
+				+ (dist[0] + dist[1]) + ")");
+		System.out.println("Rate of changes: " + rateChanges);
+		System.out.println("Rate of indels: " + rateIndels);
+	}
 
-		case 10:
-			new TestReadGenerator(path, pathlect, numberOfSequences, odist, numberOfreads, dist,
-					Double.parseDouble(args[8]), Double.parseDouble(args[9]));
-			break;
-
-		default:
-			throw new Exception("to many parameters");
+	private static String addToPathName(String foo, String extra) {
+		String[] aux = foo.split("\\.");
+		StringBuilder sb = new StringBuilder();
+		sb.append(aux[0]);
+		if (aux.length > 1) {
+			for (int i = 1; i < aux.length - 1; i++)
+				sb.append("." + aux[i]);
+			sb.append(extra);
+			sb.append("." + aux[aux.length - 1]);
+			return sb.toString();
 		}
+		return foo + extra;
 	}
 }
