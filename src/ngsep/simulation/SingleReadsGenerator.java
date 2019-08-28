55fba36f1d2371e7b0865ec16575c8d3d2b85407
jc.bojaca <jc.bojaca@LAPTOP-V19LF2JN>
2019-08-28 10:13:36 -0500

    some changes to manage the sort in the GraphBuilder

--- src/ngsep/simulation/SingleReadsGenerator.java
+++ src/ngsep/simulation/SingleReadsGenerator.java
@@ -5,17 +5,17 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.PrintStream;
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
+import java.util.PriorityQueue;
 import java.util.Random;
 import java.util.TreeSet;
+import java.util.AbstractMap.SimpleEntry;
+import java.util.Map.Entry;
 
 import ngsep.assembly.Assembler;
-import ngsep.assembly.SimplifiedAssemblyGraph;
 import ngsep.sequences.DNASequence;
+import ngsep.sequences.QualifiedSequence;
+import ngsep.sequences.io.FastaSequencesHandler;
 
 public class SingleReadsGenerator {
 	private final static double DEF_SUBSTITUTION_ERROR_RATE = 0.02;
@@ -23,9 +23,6 @@ public class SingleReadsGenerator {
 	private final static Random rnd = new Random();
 
 	public static void main(String[] args) throws Exception {
-		if (args.length < 8)
-			throw new Exception("Faltan Parametros para generar los archivos");
-
 		String path = args[0];
 		String pathlect = args[1];
 		int numberOfSequences = Integer.parseInt(args[2]);
@@ -34,32 +31,23 @@ public class SingleReadsGenerator {
 		int[] dist = new int[] { Integer.parseInt(args[6]), Integer.parseInt(args[7]) };
 		double rateChanges = (args.length > 8) ? Double.parseDouble(args[8]) : DEF_SUBSTITUTION_ERROR_RATE;
 		double rateIndels = (args.length > 9) ? Double.parseDouble(args[9]) : DEF_INDEL_ERROR_RATE;
-		String pathGraph = (args.length > 10) ? args[10] : null;
-		int minOveralp = (args.length > 11) ? Integer.parseInt(args[11]) : 0;
 
-		printInfo(path, pathlect, numberOfSequences, odist, numberOfreads, dist, rateChanges, rateIndels, pathGraph,
-				minOveralp);
-		calculate(path, pathlect, numberOfSequences, odist, numberOfreads, dist, rateChanges, rateIndels, pathGraph,
-				minOveralp);
+		printInfo(path, pathlect, numberOfSequences, odist, numberOfreads, dist, rateChanges, rateIndels);
+		calculate(path, pathlect, numberOfSequences, odist, numberOfreads, dist, rateChanges, rateIndels);
 	}
 
 	private static void printInfo(String path, String pathlect, int numberOfSequences, int[] odist, int numberOfreads,
-			int[] dist, double rateChanges, double rateIndels, String pathGraph, int minOveralp) {
+			int[] dist, double rateChanges, double rateIndels) {
 		System.out.println("Reference path: " + path);
 		System.out.println("Samples path: " + pathlect);
 		System.out.println("Reference:" + numberOfSequences + "   ~N(mean: " + odist[0] + ", sdev: " + odist[1] + ")");
 		System.out.println("Samples:" + numberOfreads + "   ~N(mean: " + dist[0] + ", sdev: " + dist[1] + ")");
 		System.out.println("Rate of changes: " + rateChanges);
 		System.out.println("Rate of indels: " + rateIndels);
-		if (pathGraph != null) {
-			System.out.println();
-			System.out.println("Graph Path: " + pathGraph);
-			System.out.println("Minimun overlap in the graph: " + minOveralp);
-		}
 	}
 
 	private static void calculate(String path, String pathlect, int numberOfSequences, int[] odist, int numberOfreads,
-			int[] dist, double rateChanges, double rateIndels, String pathGraph, int minOveralp) throws IOException {
+			int[] dist, double rateChanges, double rateIndels) throws IOException {
 
 		String[] refs = new String[numberOfSequences];
 		int[] lenRefs = normalValues(numberOfSequences, odist[0], odist[1]);
@@ -81,13 +69,28 @@ public class SingleReadsGenerator {
 		}
 
 		Assembler.exportToFile(path, "ref", refs);
-		Assembler.exportToFile(pathlect, "lect", lects);
+		exportToFileSorted(pathlect, lects, refLects, posLects, revLects);
+	}
 
-		if (pathGraph != null) {
-			SimplifiedAssemblyGraph sag = getGraph(lects, minOveralp, refLects, posLects, lenLects, revLects);
-			sag.save(pathGraph);
+	private static void exportToFileSorted(String pathlect, String[] lects, int[] refLects, int[] posLects,
+			boolean[] revLects) throws FileNotFoundException {
+		FastaSequencesHandler handler = new FastaSequencesHandler();
+		List<QualifiedSequence> list = new ArrayList<QualifiedSequence>();
+		PriorityQueue<Entry<Integer, Integer>> heap = new PriorityQueue<>(
+				(Entry<Integer, Integer> l1, Entry<Integer, Integer> l2) -> l2.getKey() - l1.getKey());
+		for (int i = 0; i < lects.length; i++)
+			heap.add(new SimpleEntry<>(lects[i].length(), i));
+
+		int j = 0;
+		while (!heap.isEmpty()) {
+			int ind = heap.poll().getValue();
+			list.add(new QualifiedSequence(
+					"lect" + (++j) + "-" + refLects[ind] + "-" + posLects[ind] + "-" + revLects[ind], lects[ind]));
 		}
 
+		try (PrintStream pr = new PrintStream(new FileOutputStream(pathlect))) {
+			handler.saveSequences(list, pr, 1000);
+		}
 	}
 
 	public static int[] normalValues(int samples, int mean, int strDesv) {
@@ -140,70 +143,6 @@ public class SingleReadsGenerator {
 			ans.add(j);
 		}
 		return ans.toArray(new Integer[0]);
-	}
-
-	private static SimplifiedAssemblyGraph getGraph(String[] lects, int minOveralp, int[] refLects, int[] posLects,
-			int[] lenLects, boolean[] revLects) throws FileNotFoundException {
-		SimplifiedAssemblyGraph sag = new SimplifiedAssemblyGraph(Arrays.asList(lects));
-
-		Map<Integer, List<Lect>> a = new HashMap<>();
-		for (int i = 0; i < refLects.length; i++)
-			a.computeIfAbsent(refLects[i], (x) -> new ArrayList<>())
-					.add(new Lect(i, lenLects[i], revLects[i], posLects[i]));
-
-		System.out.println("in");
-		for (List<Lect> list : a.values())
-			Collections.sort(list, (Lect x, Lect y) -> {
-				int ans = x.pos - y.pos;
-				if (ans != 0)
-					return ans;
-				return y.len - x.len;
-			});
-
-		System.out.println("in");
-		try (PrintStream pr = new PrintStream(new FileOutputStream("kkkk"))) {
-			for (List<Lect> list : a.values()) {
-				for (int i = 0; i < list.size() - 1; i++) {
-					Lect lect1 = list.get(i);
-					pr.println(lect1.id + ") " + lect1.pos + " " + lect1.len);
-					for (int j = i + 1; j < list.size() && list.get(j).pos < lect1.pos + lect1.len; j++) {
-						Lect lect2 = list.get(j);
-
-						int relativePos = lect2.pos - lect1.pos;
-						if (relativePos + lect2.len > lect1.len)
-							// lect1 -> lect2
-							sag.addEdge((lect1.id << 1) + (lect1.rev ? 0 : 1), (lect2.id << 1) + (lect2.rev ? 1 : 0),
-									lect1.len - relativePos, 1);
-						else {
-							// lect2 into lect1
-							boolean reversed = lect1.rev ^ lect2.rev;
-							relativePos = (lect1.rev) ? lect1.len - lect2.len - relativePos : relativePos;
-							sag.addEmbedded(lect1.id, lect2.id, relativePos, reversed, 1);
-						}
-					}
-				}
-			}
-		}
-		System.out.println("out");
-
-		sag.removeAllEmbeddedsIntoGraph();
-		System.out.println("-------------Graph Properties----------");
-		sag.printInfo();
-		System.out.println("---------------------------------------");
-
-		return sag;
-	}
-
-	private static class Lect {
-		public int id, len, pos;
-		public boolean rev;
-
-		public Lect(int id, int len, boolean rev, int pos) {
-			this.id = id;
-			this.len = len;
-			this.rev = rev;
-			this.pos = pos;
-		}
 	}
 
 }
