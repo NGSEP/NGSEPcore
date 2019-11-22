8e9c5ba2a87a438c48dff856a9504ea3b7ed050c
ap3rg <ap3rg@virginia.edu>
2019-11-22 12:55:42 -0500

    VCFtranslator. Issues with recognizing SNPs. Still working on it.

--- src/ngsep/gbs/VCFTranslator.java
+++ src/ngsep/gbs/VCFTranslator.java
@@ -6,11 +6,16 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Iterator;
+import java.util.List;
 import java.util.Map;
 
 import ngsep.alignments.ReadAlignment;
 import ngsep.alignments.io.ReadAlignmentFileReader;
+import ngsep.genome.GenomicRegion;
+import ngsep.genome.ReferenceGenome;
 import ngsep.main.CommandsDescriptor;
+import ngsep.sequences.DNASequence;
+import ngsep.variants.CalledGenomicVariant;
 import ngsep.variants.CalledSNV;
 import ngsep.variants.GenomicVariant;
 import ngsep.variants.SNV;
@@ -20,15 +25,20 @@ import ngsep.vcf.VCFFileWriter;
 import ngsep.vcf.VCFRecord;
 
 public class VCFTranslator {
-	private String filenameConsensusFasta;
+
 	private String outPrefix="./output";
 	private String filenameAlignmentBAM;
 	private String filenameRelativeVCF;
+	private static ReferenceGenome refGenome;
 	private Map<String, ReadAlignment> alignmentsHash;
 	int numTranslatedRecords = 0;
 	int totalRecords = 0;
 	int skippedRecords = 0;
 	int nullReadName = 0;
+	int notDNA = 0;
+	int TruePosNotAlign = 0;
+	int unmappedRead = 0;
+	int notSNV = 0;
 	private static Map<Character, Character> complement = new HashMap<>();
 
 	public static void main(String[] args) throws Exception {
@@ -36,9 +46,9 @@ public class VCFTranslator {
 		int i = CommandsDescriptor.getInstance().loadOptions(instance, args);
 		instance.filenameAlignmentBAM = args[i++];
 		instance.filenameRelativeVCF = args[i++];
+		String refGenomeFile = args[i++];
 		instance.outPrefix = args[i++];
-		createComplementMap();
-		instance.run();
+		instance.run(refGenomeFile);
 	}
 
 	private static void createComplementMap() {
@@ -48,38 +58,63 @@ public class VCFTranslator {
 		complement.put('G', 'C');
 
 	}
+	/**
+	 * Load reference genome based on provided file
+	 * @param genomeFileName
+	 * @return
+	 * @throws IOException
+	 */
+	private static ReferenceGenome loadReferenceGenome(String genomeFileName) throws IOException {
+		ReferenceGenome genome = new ReferenceGenome(genomeFileName);
+		System.out.println(genome.getTotalLength());
+		return genome; 
+	}
 
-	public void run() throws Exception {
+	public void run(String refGenomeFile) throws Exception {
+		createComplementMap();
+		refGenome = loadReferenceGenome(refGenomeFile);
 		getAlignmentHash();
-		System.out.println("Loaded a total of " + alignmentsHash.size() + " records to be translated.");
+		System.out.println("Loaded a total of " + alignmentsHash.size() + " alignments.");
+//		System.out.println(refGenome.getReference(0, 16320346, 16320346));
+		
 		translate();
-		System.out.println("Translated a total of " + numTranslatedRecords + " alignments.");
-		System.out.println("Skipped a total of " + skippedRecords + ".");
-		System.out.println("Total null ReadNames " + nullReadName);
 		//explore();
 		}
+	
+	/**
+	 *  For each record on the RelativeVCF, this method translates and rewrites. 
+	 * @throws Exception
+	 */
 	private void translate() throws Exception {
 		try (ReadAlignmentFileReader bamOpenFile = new ReadAlignmentFileReader(filenameAlignmentBAM);
 				VCFFileReader vcfOpenFile = new VCFFileReader(filenameRelativeVCF);
 				PrintStream mappedVCF = new PrintStream(outPrefix+"_mapped.vcf");
 				PrintStream log = new PrintStream(outPrefix + "_translation.log");) {
+			
 			boolean writeHeader = true;
 			VCFFileWriter writer = new VCFFileWriter ();
 			Iterator<VCFRecord> vcfReader = vcfOpenFile.iterator();
+			
+			// Iterate over vcfRecords
 			while(vcfReader.hasNext()) {
 				VCFRecord translatedRecord = null;
 				VCFRecord record = vcfReader.next();
+				
+				// Write header on translatedVCF
 				if(writeHeader) {
 					writeHeader = false;
 					VCFFileHeader header = record.getHeader();
 					writer.printHeader(header, mappedVCF);
 				}
+				
 				String clusterID = record.getSequenceName().split("_")[1];
 				ReadAlignment alignments =  alignmentsHash.get(clusterID);
 				if(alignments != null ) {
 					// If variant is SNP
 					if(record.getVariant().isSNV()) {
 						translatedRecord = translateRecord(alignments, record);
+					} else {
+						notSNV++;
 					}
 					if(translatedRecord != null) {
 						writer.printVCFRecord(translatedRecord, mappedVCF);
@@ -90,90 +125,143 @@ public class VCFTranslator {
 				}
 				totalRecords++;
 			}
+			log.print("Total number of records in relative VCF: " + totalRecords + "\n" +
+					"Number of translated records: " + numTranslatedRecords + "\n" +
+					"Number of skipped records due to lack of a primary alignment: " + skippedRecords + "\n" +
+					"Not DNA: " + notDNA + "\n" + 
+					"True pos not aligned to ref: " + TruePosNotAlign +"\n"+
+					"Unmapped read: " + unmappedRead + "\n" +
+					"Not SNV: " + notSNV);
 		}
 	}
 	
+	/**
+	 * This method takes in an alignment to the reference and a record 
+	 * and translates each record to match the reference.
+	 * @param algn
+	 * @param record
+	 * @return translated record
+	 */
 	private VCFRecord translateRecord(ReadAlignment algn, VCFRecord record) {
 		
 		VCFRecord translatedRecord = null;
-		// Position of alignment with respect to reference forward strand (1-based)
-		int refPos;
+		List<CalledGenomicVariant> trueCalls = new ArrayList<>();
+
 		// True position of variant with respect to reference forward strand (1-based)
 		int truePos;
+		
 		// Reference nucleotide as interpreted by the alignment to the reference
 		char trueRef;
-		// Position of variant with respect to the de-novo alignment
+		
+		// Position of variant with respect to the de-novo alignment 1-based
+		// Reference base (the base at position of variant in the consensus seq)
 		int relativePos = record.getFirst();
-		// Reference nucleotide as interpreted by the de-novo alignment
-		char relativeRef;
-		byte genotype = 0;
+		char relativeRef = algn.getReadCharacters().charAt(relativePos - 1);
+		
+		//-1 for undecided, 0 for homozygous reference, 1 for heterozygous, 2 for homozygous variant
+		byte genotype = -1;
+		
+		boolean agreement = false;
+		
 		// Variant as found in de-novo vcf
 		GenomicVariant relativeVar = record.getVariant();
+		
 		// Alternative Allele based on de-novo VCF
 		String[] relativeAlleles = relativeVar.getAlleles();
-		char relativeAlt = relativeAlleles[1].charAt(0);
+		
 		VCFFileHeader header = record.getHeader();
 		int[] filedsFormat = record.getFieldsFormat();
 		char trueAlt = 'N';
+		
+		//TODO: set up logic to deal with triallelic
 		boolean isTriallelic = false;
+		
+		// IF the reference is not in the relativeAlleles, the variant is likely triallelic
 		boolean refInRelativeAllels = false;
-	
 		
-		int read_length = algn.getReadCharacters().length();
+		// Will be something like Cluster-####
 		String seqName = algn.getSequenceName();
-		truePos = algn.getReferencePosition(relativePos);
-		CharSequence alleleCall = algn.getAlleleCall(truePos);
 		
-		// if alleleCall is null, then refChar == readChar at truePos
-		// if there is variant, we must get char from the ref-genome. 
-		if(alleleCall == null) {
-			relativeRef = algn.getReadCharacters().charAt(relativePos);
+		if(algn.isReadUnmapped()) {
+			unmappedRead++;
+			return null;
+		}
+		
+		//-1 if the read position does not align with the reference
+		truePos = algn.getReferencePosition(relativePos);
+		if(truePos == -1) {
+			TruePosNotAlign++;
+			return null;
+		}
+		
+		// get the calls for this record.
+		List<CalledGenomicVariant> calls = record.getCalls();
+//		System.out.println(truePos);
+		// TODO: Check index. Not clear if Im using it correctly
+		CharSequence trueRefSeq = refGenome.getReference(0, truePos, truePos);
+		if(!DNASequence.isDNA(trueRefSeq)) {
+			notDNA++;
+			return null;
 		} else {
-			//TODO: get actual relativeRef
-			relativeRef = algn.getReadCharacters().charAt(relativePos);
+			trueRef = trueRefSeq.charAt(0);
+		}
+		if(trueRef == relativeRef) {
+			agreement = true;
+		} else {
+			agreement = false;
 		}
 		
 		if(algn.isNegativeStrand()) {
-			trueRef = complement.get(relativeRef);
-		} else {
-			trueRef = relativeRef;
+			trueRef = complement.get(trueRef);
+		} 
+		
+		for(String allel: relativeAlleles) {
+			if(allel == Character.toString(trueRef)) {
+				refInRelativeAllels = true;
+				continue;
+			} else {
+				trueAlt = allel.charAt(0);
+			}
 		}
-		// is biallelic in relativeVCF
-		if(relativeVar.isBiallelic()) {
-			for(String allel: relativeAlleles) {
-				if(allel == Character.toString(trueRef)) {
-					refInRelativeAllels = true;
-					continue;
-				} else {
-					trueAlt = allel.charAt(0);
-				}
-			}
-			if(!refInRelativeAllels) {
-				isTriallelic = true;
-			}
+		if(!refInRelativeAllels) {
+			isTriallelic = true;
 		}
 		// Exclude reference from allels
 		if(seqName != null && (!isTriallelic)) {
 			SNV variant = new SNV(seqName, truePos, trueRef, trueAlt);
-			CalledSNV call = new CalledSNV(variant, genotype);
-			translatedRecord = new VCFRecord(variant, filedsFormat, call, header);
+			
+			// Translate genotypes 
+			//-1 for undecided, 0 for homozygous reference, 1 for heterozygous, 2 for homozygous variant
+			for(CalledGenomicVariant relativeCall: calls) {
+				if(relativeCall.isHomozygousReference() && agreement) {
+					genotype = 0;
+				} else if(relativeCall.isHomozygousReference() && !agreement) {
+					genotype = 2;
+				} else if(relativeCall.isHomozygous() && agreement) {
+					genotype = 2;
+				} else if(relativeCall.isHomozygous() && !agreement) {
+					genotype = 0;
+				} else if(relativeCall.isHeterozygous()) {
+					genotype = 1;
+				} else {
+					genotype = -1;
+				}
+				
+				CalledGenomicVariant trueCall = new CalledSNV(variant, genotype);
+				trueCalls.add(trueCall);
+			}
+			translatedRecord = new VCFRecord(variant, filedsFormat, trueCalls, header);
+		
 		} else {
 			nullReadName++;
 		}
 		return translatedRecord;
 	}
 	
-	private void explore() throws IOException {
-		try (VCFFileReader vcfOpenFile = new VCFFileReader(filenameRelativeVCF);) {
-			Iterator<VCFRecord> VCFReader = vcfOpenFile.iterator();
-			while(VCFReader.hasNext()) {
-				VCFRecord algn = VCFReader.next();
-				//System.out.print("Read Name: " + algn.getReadName());
-				System.out.print("Sequence Name: " + algn.getSequenceName() + "\n");
-			}
-		}
-	}
-
+	/**
+	 * This method builds a hash with the provided alignments to be consulted at translation time
+	 * @throws IOException
+	 */
 	private void getAlignmentHash() throws IOException {
 		this.alignmentsHash = new HashMap<>();
 		try (ReadAlignmentFileReader bamOpenFile = new ReadAlignmentFileReader(filenameAlignmentBAM);) {
@@ -187,6 +275,18 @@ public class VCFTranslator {
 			}
 		}
 		
+	}
+	
+	
+	private void explore() throws IOException {
+		try (VCFFileReader vcfOpenFile = new VCFFileReader(filenameRelativeVCF);) {
+			Iterator<VCFRecord> VCFReader = vcfOpenFile.iterator();
+			while(VCFReader.hasNext()) {
+				VCFRecord algn = VCFReader.next();
+				//System.out.print("Read Name: " + algn.getReadName());
+				System.out.print("Sequence Name: " + algn.getSequenceName() + "\n");
+			}
+		}
 	}
 	
 }
