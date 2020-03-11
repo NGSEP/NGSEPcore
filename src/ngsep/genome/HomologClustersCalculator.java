2c32378468f22e833af046477a2f3949251b6622
Jorge Gomez <jagv1998@gmail.com>
2020-03-10 20:42:01 -0500

    Added sparse matrix and built it for the input data

--- src/ngsep/genome/HomologClustersCalculator.java
+++ src/ngsep/genome/HomologClustersCalculator.java
@@ -2,6 +2,7 @@ package ngsep.genome;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collection;
 import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
@@ -9,10 +10,11 @@ import java.util.Map;
 import java.util.Queue;
 import java.util.logging.Logger;
 
+import ngsep.genome.strucs.SparseMatrix;
 import ngsep.math.Distribution;
 
 public class HomologClustersCalculator {
-
+ 
 	private Logger log;
 	
 	public Logger getLog() {
@@ -26,52 +28,31 @@ public class HomologClustersCalculator {
 	public List<List<HomologyUnit>> clusterHomologs(List<AnnotatedReferenceGenome> genomes, List<HomologyEdge> homologyEdges) {
 		log.info("Clustering orthologs and paralogs");
 		List<List<HomologyUnit>> orthologyUnitClusters=new ArrayList<>();
-		List<HomologyUnit> unitsWithOrthologs = new ArrayList<>();
-		Map<String, Integer> unitsPositionMap = new HashMap<>();
-		for(AnnotatedReferenceGenome genome:genomes) {
-			for(HomologyUnit unit:genome.getHomologyUnits()) {
-				if(unit.getTotalHomologs()>0) {
-					unitsPositionMap.put(unit.getUniqueKey(), unitsWithOrthologs.size());
-					unitsWithOrthologs.add(unit);
-				}
-
+		
+		List<HomologyUnit> reference = new ArrayList<>();
+		HashMap<String, Integer> indexOf = new HashMap<>();
+		for (AnnotatedReferenceGenome genome : genomes) {
+			for (HomologyUnit unit: genome.getHomologyUnits()) {
+				String newId = String.format("%s___%s", genome.getId(), unit.getId());
+				if(indexOf.containsKey(newId)) log.info("The id combination is already in use. This is bad, very very bad.");
+				indexOf.put(newId, reference.size());
+				reference.add(unit);
 			}
 		}
-		log.info("Total units with orthologs: "+unitsWithOrthologs.size());
-		int [] group = new int [unitsWithOrthologs.size()];
-		Arrays.fill(group, -1);
-
-		Distribution distClusterSizes = new Distribution(0, 50, 1);
-		//Build connected components
-		for(int i=0;i<group.length;i++) {
-			if(group[i]!=-1) continue;
-			List<HomologyUnit> cluster = new ArrayList<>();
-			int groupNumber = orthologyUnitClusters.size();
-			Queue<HomologyUnit> agenda = new LinkedList<>();
-			agenda.add(unitsWithOrthologs.get(i));
-			while(agenda.size()>0) {
-				HomologyUnit unit = agenda.remove();
-				int pos = unitsPositionMap.get(unit.getUniqueKey());
-				int unitGroup = group[pos];
-				if(unitGroup==-1) {
-					cluster.add(unit);
-					group[pos]=groupNumber;
-					//agenda.addAll(unit.getOrthologsAllGenomes());
-				} else if(unitGroup!=groupNumber) log.warning("Possible connection between clusters "+unitGroup + " and "+groupNumber+" Unit: "+unit.getUniqueKey());
-
+		
+		SparseMatrix similarityMatrix = new SparseMatrix(reference.size(), reference.size());
+		for (int i = 0; i < reference.size(); i++) {
+			HomologyUnit unit = reference.get(i);
+			Collection<HomologyEdge> edges = unit.getAllHomologyRelationships();
+			for(HomologyEdge edge : edges) {
+				String id_composed = String.format("%s___%s", edge.getSubjectUnit().getGenomeId(), edge.getSubjectUnit().getId());
+				similarityMatrix.set(i, indexOf.get(id_composed), edge.getScore());
 			}
-			if(cluster.size()>1) {
-				orthologyUnitClusters.add(cluster);
-				distClusterSizes.processDatapoint(cluster.size());
-			} else if (cluster.size()==0) {
-				log.warning("Empty cluster from unit: "+unitsWithOrthologs.get(i).getUniqueKey()+" clusters: "+orthologyUnitClusters.size()+" groupNumber: "+groupNumber);
-			}
-
 		}
-		log.info("Number of clusters: "+orthologyUnitClusters.size());
-		//TODO: Report it better
-		distClusterSizes.printDistributionInt(System.out);
+		
+		log.info("==== Similarity matrix ====");
+		log.info(similarityMatrix.getMatrixAsString());
+		
 		return orthologyUnitClusters;
 	}
-
 }
---
+++ src/ngsep/genome/strucs/SparseMatrix.java
@@ -0,0 +1,79 @@
+package ngsep.genome.strucs;
+
+import java.util.HashMap; 
+
+public class SparseMatrix {
+	private SparseVector[] vectors;
+	
+	public SparseMatrix(int length, int height) {
+		vectors = new SparseVector[length];
+		for (int i = 0; i < length; i++) vectors[i] = new SparseVector(height);
+	}
+	
+	public double get(int i, int j) {
+		return vectors[i].get(j);
+	}
+	
+	public void set(int i, int j, double k) {
+		vectors[i].set(j, k);
+	}
+	
+	public String getLineAsString(int i) {
+		String line = "[";
+		for (int j = 0; j < vectors.length; j++) {
+			line = line.concat(String.format(" %f,", this.get(i, j)));
+		}
+		line = line.substring(0,line.length() - 1);
+		line = line.concat("]");
+		return line;
+	}
+	
+	public String getMatrixAsString() {
+		String matrix = "";
+		for (int i = 0; i < vectors.length; i++) {
+			matrix = matrix.concat(String.format("%s\n", this.getLineAsString(i)));
+		}
+		return matrix;
+	}
+	
+	class SparseVector {
+		private int length = 0;  
+		private HashMap<Integer, Double> vector;
+		
+		public SparseVector(int length) {
+			vector = new HashMap<>();
+			this.length = length;
+		}
+		
+		public void set(int pos, double k) {
+			if (pos < 0 || pos >= length) {
+				throw new IndexOutOfBoundsException();
+			} else {
+				vector.put(pos, k);
+				length++;
+			}
+		}
+		
+		public double get(int pos) {
+			if (pos < 0 || pos >= length) {
+				throw new IndexOutOfBoundsException();
+			} else {
+				if (vector.containsKey(pos)) {
+					return vector.get(pos);
+				} else {
+					return 0;
+				}
+			}
+		}
+		
+		public void append(double k) {
+			vector.put(length++, k);
+		}
+	}
+	
+	public static void main(String[] args) {
+		SparseMatrix m = new SparseMatrix(5, 5);
+		m.set(1, 4, 10.47);
+		System.out.println(m.getMatrixAsString());
+	}
+}
