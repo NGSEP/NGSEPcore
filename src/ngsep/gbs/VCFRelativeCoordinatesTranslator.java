07408d0be00cbfb35773abfd9cbf75a895c2b357
ap3rg <ap3rg@virginia.edu>
2020-04-10 13:15:09 -0500

    Fixed VCFTrasnlator

--- src/ngsep/gbs/VCFRelativeCoordinatesTranslator.java
+++ src/ngsep/gbs/VCFRelativeCoordinatesTranslator.java
@@ -218,7 +218,7 @@ public class VCFRelativeCoordinatesTranslator {
 		}
 		
 		CharSequence trueRefSeq = refGenome.getReference(algnIndex, truePos, truePos);
-		System.out.println(trueRefSeq + "\t" + truePos + "\t" + algn.getReadLength());
+		System.out.println(trueRefSeq + "\t" + truePos + "\t" + refGenome.getTotalLength());
 		
 		if(!DNASequence.isDNA(trueRefSeq)) {
 			notDNA++;
