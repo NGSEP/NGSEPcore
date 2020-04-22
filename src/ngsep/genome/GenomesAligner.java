29ef574304953068a7794c19fbc58a6630abe86d
Jorge Gomez <jagv1998@gmail.com>
2020-04-22 13:43:58 -0500

    forgot new parameter

--- src/ngsep/genome/GenomesAligner.java
+++ src/ngsep/genome/GenomesAligner.java
@@ -194,7 +194,7 @@ public class GenomesAligner {
 		}
 		HomologClustersCalculator calculator = new HomologClustersCalculator();
 		calculator.setLog(log);
-		orthologyUnitClusters = calculator.clusterHomologs(genomes, homologyEdges);
+		orthologyUnitClusters = calculator.clusterHomologs(genomes, homologyEdges, skipMCL);
 		if(genomes.size()<2) return;
 		// By now this is still done for two genomes
 		AnnotatedReferenceGenome genome1 = genomes.get(0);
