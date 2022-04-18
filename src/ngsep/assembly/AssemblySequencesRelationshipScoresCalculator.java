a25cdf1c4f93be1f0f22ea466bfb6c156085c6b6
jorge <jorge@jorge-HP-Spectre-x360-Convertible>
2022-04-17 19:39:32 -0500

    Equalized weights of overlap and WCSK

--- src/ngsep/assembly/AssemblySequencesRelationshipScoresCalculator.java
+++ src/ngsep/assembly/AssemblySequencesRelationshipScoresCalculator.java
@@ -79,7 +79,7 @@ public class AssemblySequencesRelationshipScoresCalculator {
 		int maxIndividualCost = 10;
 		double w = weightsSecondaryFeatures;
 		double [] individualCosts = new double[6];
-		double [] limitPValues = {1,0.5,0.1,0.05,0.5,0.25};
+		double [] limitPValues = {1,1,0.1,0.05,0.5,0.25};
 		double [] weights      = {1,  1,0,   0,0.5,w};
 		
 		double cumulativeOverlap = overlapD.cumulative(relationship.getOverlap());
