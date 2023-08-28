20b83a7ca5e7f06405fb1abc70beb36f9eb808b4
acastem15 <85703757+acastem15@users.noreply.github.com>
2023-08-28 13:10:08 -0500

    banded with test

--- src/ngsep/alignments/PairwiseAlignerStaticBanded.java
+++ src/ngsep/alignments/PairwiseAlignerStaticBanded.java
@@ -1,6 +1,7 @@
 package ngsep.alignments;
 
 import java.util.HashMap;
+import java.util.Objects;
 
 import ngsep.sequences.LimitedSequence; 
 
@@ -8,74 +9,105 @@ public class PairwiseAlignerStaticBanded implements PairwiseAligner {
 
     private int match =1; 
 
-    private int mismatch = -1; 
+    private int mismatch = 1; 
 
-    private int  indel = 0; 
+    private int  indel = 2; 
 
     private HashMap<Tuple,Integer> nodes = new HashMap<>(); 
 
-    private int k = 0; 
+    private int k = 3; 
+    
+    
+   
 
-    @Override
+    public int getK() {
+		return k;
+	}
+
+	public void setK(int k) {
+		this.k = k;
+	}
+
+	@Override
     public String[] calculateAlignment(CharSequence sequence1, CharSequence sequence2 ) {
-        // sequence2.length()> sequence1.length()-> Right definition of 
+        // sequence1.length()> sequence2.length()-> Right definition of 
 
-        if (checkminK(sequence1, sequence2, k)){
-            calculateHashMap(sequence1, sequence2, k);
-            return aligenedSequences(sequence1, sequence2, k); 
+        if (checkminK(sequence1, sequence2)){
+            calculateHashMap(sequence1, sequence2);
+            return aligenedSequences(sequence1, sequence2); 
         }else{
             throw new RuntimeException("K value is not possible"); 
         }
-
-       
-
-
     }
+	
+	public int getMaxScore(CharSequence sequence1, CharSequence sequence2 ) {
+		
+			return nodes.get(new Tuple(sequence1.length(),sequence2.length())); 
 
-    private void calculateHashMap(CharSequence sequence1, CharSequence sequence2, int k ){
+	}
+	
+    private void calculateHashMap(CharSequence sequence1, CharSequence sequence2){
+    	 int i = sequence1.length();
+         int j = sequence2.length();
+         
+
         //Set node 0,0 
         nodes.put(new Tuple(0,0), 0); 
         //Set other nodes
-        for (int row = 0; row <sequence1.length(); row++ ){
-            for (int col = Math.max(0,row-k) ; col<Math.min(k+row+1, sequence2.length()); col++){
-                Tuple actual = new Tuple(row,col); 
-                int valDiag = nodes.get(new Tuple(row-1,col-1)); 
-                int valIzq  = nodes.get(new Tuple(row, col-1));
-                int valUp = nodes.get(new Tuple (row-1, col)); 
-                if (row == 0 && col == 0 ){
+        for (int row = 0; row <sequence1.length()+1; row++ ){
+            for (int col = Math.max(0,row-this.k) ; col<Math.min(k+row+1, sequence2.length()+1); col++){
+            	if (row == 0 && col == 0 ){
                  
-                }else if (row == 0 && col <=k ){
-                    nodes.put(actual, valIzq+ insertionCost(sequence1.charAt(row), sequence2.charAt(col-1))); 
+                }else if (row == 0 && col <=this.k ){
+                	
+                    nodes.put(new Tuple(row,col), nodes.get(new Tuple(row, col-1))+ insertionCost(sequence1.charAt(row), sequence2.charAt(col-1))); 
 
                 }else if (col == 0 && row != 0 ){
-                    nodes.put(actual, valUp+deletionCost(sequence1.charAt(row-1), sequence2.charAt(col))); 
+                	
+                    nodes.put(new Tuple(row,col), nodes.get(new Tuple (row-1, col))+deletionCost(sequence1.charAt(row-1), sequence2.charAt(col))); 
 
+                }else if(col-row ==this.k){
+       
+                	
+                	int maxNum = Math.max(nodes.get(new Tuple(row-1,col-1))+matchMismatchCost(sequence1.charAt(row-1), sequence2.charAt(col-1)), 
+      					  				  nodes.get(new Tuple(row, col-1))+insertionCost(sequence1.charAt(row-1), sequence2.charAt(col-1)));
+                	nodes.put(new Tuple(row,col),maxNum); 
+          
+                	
+                	
+                }else if(row-col==this.k){
+                	
+                	int maxNum = Math.max(nodes.get(new Tuple(row-1,col-1))+matchMismatchCost(sequence1.charAt(row-1), sequence2.charAt(col-1)), 
+                						  nodes.get(new Tuple (row-1, col))+deletionCost(sequence1.charAt(row-1), sequence2.charAt(col-1)));
+                	nodes.put(new Tuple(row,col),maxNum); 
+                	
                 }else{
-                    int maxNum = Math.max(valDiag+matchMismatchCost(sequence1.charAt(row-1), sequence2.charAt(col)), 
-                                            valIzq+insertionCost(sequence1.charAt(row), sequence2.charAt(col-1))); 
-                    maxNum = Math.max(maxNum, valUp+deletionCost(sequence1.charAt(row-1), sequence2.charAt(col))); 
-
-                    nodes.put(actual,maxNum); 
+         
+                	
+                    int maxNum = Math.max(nodes.get(new Tuple(row-1,col-1))+matchMismatchCost(sequence1.charAt(row-1), sequence2.charAt(col-1)), 
+                    					  nodes.get(new Tuple(row, col-1))+insertionCost(sequence1.charAt(row-1), sequence2.charAt(col-1))); 
+                    maxNum = Math.max(maxNum, nodes.get(new Tuple (row-1, col))+deletionCost(sequence1.charAt(row-1), sequence2.charAt(col-1))); 
+                    nodes.put(new Tuple(row,col),maxNum); 
 
                 }
 
             }
         }
+
     }
 
-    private String[] aligenedSequences(CharSequence sequence1, CharSequence sequence2, int k ){
+    private String[] aligenedSequences(CharSequence sequence1, CharSequence sequence2){
 
         int i = sequence1.length();
         int j = sequence2.length();
+        
+
 
         StringBuffer ns1 = new StringBuffer();
 		StringBuffer ns2 = new StringBuffer();
 
         while (j>0 || i>0){
             int actual = nodes.get(new Tuple(i,j)); 
-            int valDiag = nodes.get(new Tuple(i-1,j-1)); 
-            int valIzq  = nodes.get(new Tuple(i, j-1));
-            int valUp = nodes.get(new Tuple (i-1, j)); 
             if (i==0 && j!=0){
                 ns1.append(LimitedSequence.GAP_CHARACTER); 
                 ns2.append(sequence2.charAt(j-1)); 
@@ -84,21 +116,36 @@ public class PairwiseAlignerStaticBanded implements PairwiseAligner {
                 ns1.append(sequence1.charAt(i-1)); 
                 ns2.append(LimitedSequence.GAP_CHARACTER); 
                 i--; 
-            }else if (actual == valDiag+matchMismatchCost(sequence1.charAt(i-1), sequence2.charAt(j-1))){
-                ns1.append(sequence1.charAt(i-1)); 
+            }else if (actual == nodes.get(new Tuple(i-1,j-1))+matchMismatchCost(sequence1.charAt(i-1), sequence2.charAt(j-1))){
+                
+            	
+            	ns1.append(sequence1.charAt(i-1)); 
                 ns2.append(sequence2.charAt(j-1)); 
                 j--; 
                 i--; 
-            }else if ((j-1 == k)&&(actual == valIzq+insertionCost(sequence1.charAt(i), sequence2.charAt(j-1)) )){
+            }else if ((j-i == this.k)&&(actual == nodes.get(new Tuple(i, j-1))+insertionCost(sequence1.charAt(i-1), sequence2.charAt(j-1)))){
                 ns1.append(LimitedSequence.GAP_CHARACTER); 
                 ns2.append(sequence2.charAt(j-1)); 
                 j--;
-            }else if ((i-j == k ) && (actual == valUp+deletionCost(sequence1.charAt(i), sequence2.charAt(j-1)))){
+            }else if ((i-j == this.k ) && (actual == nodes.get(new Tuple (i-1, j))+deletionCost(sequence1.charAt(i-1), sequence2.charAt(j-1)))){
                 ns1.append(sequence1.charAt(i-1)); 
                 ns2.append(LimitedSequence.GAP_CHARACTER); 
                 i--; 
             }
-            else throw new RuntimeException("Unexpected score error at "+i+" "+j); 
+            else if ((j-i != this.k) &&(i-j != this.k )) {
+            	if(actual == nodes.get(new Tuple(i, j-1))+insertionCost(sequence1.charAt(i-1), sequence2.charAt(j-1))) {
+            		ns1.append(LimitedSequence.GAP_CHARACTER); 
+                    ns2.append(sequence2.charAt(j-1)); 
+                    j--;
+            	}else if (actual == nodes.get(new Tuple (i-1, j))+deletionCost(sequence1.charAt(i-1), sequence2.charAt(j-1))) {
+            		ns1.append(sequence1.charAt(i-1)); 
+                    ns2.append(LimitedSequence.GAP_CHARACTER); 
+                    i--; 
+            		
+            	}else throw new RuntimeException("Unexpected score error at "+i+" "+j); 
+
+            	
+            }else throw new RuntimeException("Unexpected score error at "+i+" "+j); 
             }
         String[] seqs = new String[2]; 
         seqs[0] = ns1.reverse().toString();
@@ -113,11 +160,11 @@ public class PairwiseAlignerStaticBanded implements PairwiseAligner {
     
 
     private int insertionCost(char l1, char l2){
-        return indel; 
+        return -indel; 
 
     }
     private int deletionCost(char l1, char l2){
-        return indel; 
+        return -indel; 
 
     }
     private int matchMismatchCost(char l1, char l2){
@@ -125,15 +172,15 @@ public class PairwiseAlignerStaticBanded implements PairwiseAligner {
             return match;
 
         }else{
-            return mismatch; 
+            return -mismatch; 
         }
 
     }
 
 
 
-    private boolean checkminK(CharSequence sequence1, CharSequence sequence2, int k ){
-        return (k>= sequence1.length()-sequence2.length());
+    private boolean checkminK(CharSequence sequence1, CharSequence sequence2){
+        return this.k!=0 &&((this.k>= sequence1.length()-sequence2.length())||(this.k>= sequence2.length()-sequence1.length()));
     }
     
 }
@@ -164,10 +211,15 @@ class Tuple{
         return num1 == other.num1 && num2 == other.num2; 
 
     }
-
+    @Override
     public int hashCode(){
-        return this.hashCode(); 
+        return Objects.hash(num1,num2); 
     }
+
+	@Override
+	public String toString() {
+		return "Tuple [num1=" + num1 + ", num2=" + num2 + "]";
+	}
 
 
 }
--- test/ngsep/alignments/test/PairwiseAlignmentTest.java
+++ test/ngsep/alignments/test/PairwiseAlignmentTest.java
@@ -1,53 +1,259 @@
 package ngsep.alignments.test;
 
+import java.io.IOException;
+import java.util.List;
+import java.util.Random;
+
 import junit.framework.TestCase;
+
 import ngsep.alignments.PairwiseAlignerDynamicKmers;
 import ngsep.alignments.PairwiseAlignerSimpleGap;
-import ngsep.sequences.SimpleEditDistanceMeasure;
+import ngsep.alignments.PairwiseAlignerStaticBanded;
+import ngsep.sequences.DNASequence;
+import ngsep.sequences.LimitedSequence;
+import ngsep.sequences.QualifiedSequence;
+
+import ngsep.sequences.io.*;
 
 public class PairwiseAlignmentTest extends TestCase {
-	public void testPairwiseAlignmentKmers() {
-		String seq1 = "ATGGACATCGTACACAGTATGGACATTGTACACAGT";
-		String seq2 = "ATGGACAGTACATAGTATGGACAGTACATAGT";
-		SimpleEditDistanceMeasure aligner1 = new SimpleEditDistanceMeasure();
-		String [] aln1 = aligner1.calculateAlignment(seq1, seq2);
-		PairwiseAlignerDynamicKmers aligner2 = new PairwiseAlignerDynamicKmers();
-		//PairwiseAlignerSimpleGap aligner2 = new PairwiseAlignerSimpleGap();
-		String [] aln2 = aligner2.calculateAlignment(seq1, seq2);
-		System.out.println(aln1[0]+"\n"+aln1[1]);
-		System.out.println(aln2[0]+"\n"+aln2[1]);
-		//assertEquals(aln1[0], aln2[0]);
-		//assertEquals(aln1[1], aln2[1]);
+	private CharSequence seq1 = ""; 
+	private CharSequence seq2 = ""; 
+	private int match =1; 
+
+    private int mismatch = 1; 
+
+    private int  indel = 2; 
+	
+	
+	private int FindScore(CharSequence algseq1, CharSequence algseq2) {
+		int cost = 0; 
 		
-		System.err.println("Testing alignments");
-		seq1 = "GGGAAGAAAACGATGAATGTGTG";
-		seq2 = "AGAATGGGAAGAAAACGATGAATGTGTG";
-		String [] aln = aligner2.calculateAlignment(seq1, seq2);
+		for (int i = 0; i<algseq1.length(); i++) {
+			
+			if (algseq1.charAt(i) == LimitedSequence.GAP_CHARACTER) {
+				cost += insertionCost(algseq1.charAt(i),algseq2.charAt(i));
+				
+			}else if (algseq2.charAt(i) == LimitedSequence.GAP_CHARACTER) {
+				cost += deletionCost(algseq1.charAt(i),algseq2.charAt(i));
+			}else {
+				cost+=matchMismatchCost(algseq1.charAt(i),algseq2.charAt(i)); 
+			}
+		}
+		return cost; 
+		
+		
+	}
+	
+    private int insertionCost(char l1, char l2){
+        return -indel; 
+
+    }
+    private int deletionCost(char l1, char l2){
+        return -indel; 
+
+    }
+    private int matchMismatchCost(char l1, char l2){
+        if (l1 == l2){
+            return match;
+
+        }else{
+            return -mismatch; 
+        }
+
+    }
+	
+	
+	public void loadTwoSequences() {
+		String file = "./dataTest/"+"exampleFLOGenes.fa"; 
+		
+		FastaSequencesHandler reader = new FastaSequencesHandler(); 
+		reader.setSequenceType(DNASequence.class);
+		reader.setKeepLowerCase(false);
+		List<QualifiedSequence> sequences; 
+		
+		try {
+		sequences = reader.loadSequences(file);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			sequences = null; 
+		} 
+		Random rand = new Random(); 
+		int i = rand.nextInt(sequences.size()); 
+		int j = rand.nextInt(sequences.size()); 
+
+		
+		
+		seq1 = sequences.get(i).getCharacters(); 
+		seq2 = sequences.get(j).getCharacters(); 
+		
+	}
+	
+	//Create test with JUnit cases 
+	public void testPairwiseAlignmentStaticBanded() {
+		
+		//Create Aligners
+				PairwiseAlignerStaticBanded aligner1 = new PairwiseAlignerStaticBanded(); 
+				PairwiseAlignerSimpleGap aligner2 = new PairwiseAlignerSimpleGap();
+				PairwiseAlignerDynamicKmers aligner3 = new PairwiseAlignerDynamicKmers();
+				
+		//Get random sequence
+	
+
+			double deltaMax = 10*0.01;
+			double deltaMin = (0)*0.01;
+			int cases = 50; 
+
+			int smallError = 0; 
+			for (int i = 0; i<cases; i++) {
+				
+				
+				loadTwoSequences(); 
+				//System.err.println("Testing alignments");
+				//Banded Alignment
+					//Set K-band value-Positive number
+				int k = seq1.length()-seq2.length(); 
+
+				if (k<0) {
+					k = -k; 
+					aligner1.setK(k);
+				}else if (k == 0) {
+					aligner1.setK(seq1.length()/4);
+				}else {
+					aligner1.setK(k);
+					
+				}
+				
+				String []aln1 = aligner1.calculateAlignment(seq1, seq2);
+				System.out.println(aln1[0]+"\n"+aln1[1]);
+				
+				int maxScoreAligner1 = aligner1.getMaxScore(seq1, seq2);
+				//Score static banded
+	
+				maxScoreAligner1 = FindScore(aln1[0],aln1[1]); 
+
+			
+				//Simple Gap Alignment
+				String [] aln2 = aligner2.calculateAlignment(seq1,seq2);
+				System.out.println(aln2[0]+"\n"+aln2[1]);
+				
+				//Score simple gap
+				int maxScoreAligner2 = FindScore(aln2[0],aln2[1]); 
+				
+				//System.out.println(maxScoreAligner1+" "+maxScoreAligner2);
+				
+				double delta = Math.abs(maxScoreAligner2- maxScoreAligner1) ; 
+				System.out.println(delta/Math.abs(maxScoreAligner2));
+				
+				assertTrue((delta/Math.abs(maxScoreAligner2))<0.1);
+
+			}
+		
+		
+		
+		
+		
+		
+	
+		
+		
+		 
+
+		
+
+
+		
+		
+		/*
+		
+		aln1 = aligner1.calculateAlignment(seq1, seq2);
+		System.out.println(aln1[0]+"\n"+aln1[1]);
+		
+		aligner3.setK(seq2.length()-seq1.length());
+		String [] aln = aligner3.calculateAlignment(seq1, seq2);
 		System.out.println(aln[0]+"\n"+aln[1]);
 		
-		seq1 = "CACGGTTTATCCTGCATGCTCCGTCTTCT";
-		seq2 = "GATTATCTGCATGCTCCGTCTTCT";
-		aln = aligner2.calculateAlignment(seq1, seq2);
+		
+		aln1 = aligner1.calculateAlignment(seq1, seq2);
+		System.out.println(aln1[0]+"\n"+aln1[1]);
+		aligner3.setK(seq1.length()-seq2.length());
+		aln = aligner3.calculateAlignment(seq1, seq2);
 		System.out.println(aln[0]+"\n"+aln[1]);
 		
 		System.err.println("Testing weird cases");
-		seq1 = "TTTTTTTTCACTTTCTTTTTTTTTT";
-		seq2 = "GAAGGCAAATCCATTCGAACCGTAATTCGTTACTGA";
-		aln = aligner2.calculateAlignment(seq1, seq2);
-		//System.out.println(aln[0]+"\n"+aln[1]);
-		assertNull(aln);
-		seq1 =  "ACTCTTTTTCGCCAAACGGATGCGCCCTC";
-		seq2 = "TACTCTTTTTCGCCAAAACGGATGCGCCCTCT";
-		aln = aligner2.calculateAlignment(seq1, seq2);
+	
+		
+		aln1 = aligner1.calculateAlignment(seq1, seq2);
+		System.out.println(aln1[0]+"\n"+aln1[1]);
+		
+		aligner3.setK(seq2.length()-seq1.length());
+		aln = aligner3.calculateAlignment(seq1, seq2);
 		System.out.println(aln[0]+"\n"+aln[1]);
-		seq1 = "GGATTGGGGGGGTGAAGAGTGGGGGGTGGGGGAGGGGGGGGGAGCTGGGGGGGGGAAA";
-		seq2 = "GCGCAACGCGTCAGTGGGCTGATCATTAACTATCCGCTGGATGACAGGATGCCATTGCTGTGG";
-		aln = aligner2.calculateAlignment(seq1, seq2);
-		System.out.println(aln[0]+"\n"+aln[1]+" "+seq1.length());
 		//assertNull(aln);
+		
+		
+		aln1 = aligner1.calculateAlignment(seq1, seq2);
+		System.out.println(aln1[0]+"\n"+aln1[1]);
+		
+		aligner3.setK(seq2.length()-seq1.length());
+		aln = aligner3.calculateAlignment(seq1, seq2);
+		System.out.println(aln[0]+"\n"+aln[1]);
+		
+		
+		
+		aln1 = aligner1.calculateAlignment(seq1, seq2);
+		System.out.println(aln1[0]+"\n"+aln1[1]);
+		
+		aligner3.setK(seq2.length()-seq1.length());
+		aln = aligner3.calculateAlignment(seq1, seq2);
+		System.out.println(aln[0]+"\n"+aln[1]);
+		
+		//assertNull(aln);
+		
+		
+		aln1 = aligner1.calculateAlignment(seq1, seq2);
+		System.out.println(aln1[0]+"\n"+aln1[1]);
+		
+		aligner3.setK(seq1.length()-seq2.length());
+		aln = aligner3.calculateAlignment(seq1, seq2);
+		System.out.println(aln[0]+"\n"+aln[1]);
+		*/
+		
+		//Old tests
+		
+		/*
+		String seq1 = "ATGGACATCGTACACAGTATGGACATTGTACACAGT";
+		String seq2 = "ATGGACAGTACATAGTATGGACAGTACATAGT";
+		*/
+		
+		/*
 		seq1 = "TACAGTTGGAATGACCACAAATCACAAATGTCAGATACCCGAAGCGCAGCG";
 		seq2 =  "ACAGTTGGAATGACCACAAATCACAATGTCAGATACCCGAAGCGCAGC";
-		aln = aligner2.calculateAlignment(seq1, seq2);
-		System.out.println(aln[0]+"\n"+aln[1]);
+		*/
+		
+		/*
+		seq1 = "GGATTGGGGGGGTGAAGAGTGGGGGGTGGGGGAGGGGGGGGGAGCTGGGGGGGGGAAA";
+		seq2 = "GCGCAACGCGTCAGTGGGCTGATCATTAACTATCCGCTGGATGACAGGATGCCATTGCTGTGG";
+		*/
+		
+		/*
+		seq1 =  "ACTCTTTTTCGCCAAACGGATGCGCCCTC";
+		seq2 = "TACTCTTTTTCGCCAAAACGGATGCGCCCTCT";
+		*/
+		
+		/*
+		seq1 = "TTTTTTTTCACTTTCTTTTTTTTTT";
+		seq2 = "GAAGGCAAATCCATTCGAACCGTAATTCGTTACTGA";
+		 */
+		
+		/*
+		seq1 = "GGGAAGAAAACGATGAATGTGTG";
+		seq2 = "AGAATGGGAAGAAAACGATGAATGTGTG";
+		 */
+		
+		/*
+		seq1 = "CACGGTTTATCCTGCATGCTCCGTCTTCT";
+		seq2 = "GATTATCTGCATGCTCCGTCTTCT";
+		 */
 	}
 }
