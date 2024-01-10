43f898e036846eafdea3905d7a052e05e7a9e31b
jorge <jorge@jorge-OptiPlex-5090>
2024-01-10 16:36:36 -0500

    Removed contribution of evidence proportion to the score

--- src/ngsep/assembly/AssemblySequencesRelationshipScoresCalculator.java
+++ src/ngsep/assembly/AssemblySequencesRelationshipScoresCalculator.java
@@ -82,7 +82,7 @@ public class AssemblySequencesRelationshipScoresCalculator {
 		double w = weightsSecondaryFeatures;
 		double [] individualCosts = new double[6];
 		double [] limitPValues = {1,1,0.1,0.05,0.5,0.25};
-		double [] weights      = {0,  1,0,   0,0.5,w};
+		double [] weights      = {0,  1,0,   0,0,w};
 		
 		double cumulativeOverlap = overlapD.cumulative(relationship.getOverlap());
 		//if(pValueOTP>0.5) pValueOTP = 1- pValueOTP;
